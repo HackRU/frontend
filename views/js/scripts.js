5509faacdd80ba6d7f9b84cf7b88cb5531b19d6f
dchen <chendavid96@gmail.com>
2017-08-21 22:51:31 -0400

    committed and pushed on Mon Aug 21 22:51:31 EDT 2017

--- views/js/scripts.js
+++ views/js/scripts.js
@@ -4,7 +4,7 @@
 $(document).ready(function() {
 
     var pageLocation = window.location.pathname; // used to run certain JS on specific pages
-
+    console.log(pageLocation);
 
     if (pageLocation == '/' || pageLocation == '/index.ejs') { // run on home page (landing page)
         // console.log("THIS IS THE HOME PAGE.");
@@ -16,6 +16,8 @@ $(document).ready(function() {
         $('#resume-upload-input').on("change", function(){
             changeFileUploadPreview();
         });
+
+        highlightCurrentPageOnMenu();
     }
 
 
@@ -27,13 +29,26 @@ function smoothScrollEnable() {
         event.preventDefault();
         $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
     });
-    return;
 }
 
 function changeFileUploadPreview(){
     // for use in resume uploading on the registration and account management pages.
     var fileName = $("#resume-upload-input").val().replace(/\\/g, '/').replace(/.*\//, '');
     $("#file-selector-notif").text(fileName);
-    console.log(fileName);
 
+}
+
+function highlightCurrentPageOnMenu() {
+
+    var pageName = window.location.pathname;
+
+    if (pageName.includes("registration")) {
+        $('#nav-link-registration').addClass('bold-text');
+    }
+    else if (pageName.includes("dashboard")) {
+        $('#nav-link-dashboard').addClass('bold-text');
+    }
+    else {
+
+    }
 }
