dd286a3972ab05f30a7d299c7d6a3c03bc1feab1
Fan Liu <fl273@assembly.cs.rutgers.edu>
2018-01-21 23:33:16 -0500

    Purple

--- src/App.js
+++ src/App.js
@@ -3,6 +3,20 @@ import React from 'react';
 import 'whatwg-fetch'
 
 class App extends React.Component {
+  constructor (props){
+    super(props);
+    this.state = {
+      email:'',
+      password: ''
+    };
+	
+    this.login = this.login.bind(this);
+    this.signUp = this.signUp.bind(this);
+    this.mlh = this.mlh.bind(this);
+    this.onEmailChange = this.onEmailChange.bind(this);
+    this.onPasswordChange = this.onPasswordChange.bind(this);
+  }
+
   login() {
     fetch('https://m7cwj1fy7c.execute-api.us-west-2.amazonaws.com/test/authorize', {
       method: 'POST',
@@ -11,8 +25,8 @@ class App extends React.Component {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        email: 'email here',
-        password: 'password',
+        email: this.state.email,
+        password: this.state.password,
       })
     }).then(function(data){
       alert(data);
@@ -27,8 +41,8 @@ class App extends React.Component {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        email: 'email here',
-        password: 'password',
+        email: this.state.email,
+        password: this.state.password,
       })
     })
 
@@ -38,14 +52,22 @@ class App extends React.Component {
     window.location.href = "https://my.mlh.io/oauth/authorize?client_id=bab4ace712bb186d8866ff4776baf96b2c4e9c64d729fb7f88e87357e4badcba&redirect_uri=https://m7cwj1fy7c.execute-api.us-west-2.amazonaws.com/test/mlhcallback&response_type=code&scope=email+education+birthday";
   }
 
+  onEmailChange(e){
+    this.setState({email: e.target.value})
+  }
 
+  onPasswordChange(e){
+    this.setState({password: e.target.value})
+  }
 
   render() {
     return (
-      <div>
+      <div className="App">
 	<p>Enter your name and email in the fields, then click "Login" or "Sign Up". Click "MLH" to login from MLH website.</p>
 
-	
+	<form id = "fm1" action="/action_page.php">
+		Email: <input value={this.state.email} onChange={this.onEmailChange} type="email" name="email"/><br/>
+		Password: <input value={this.state.password} onChange={this.onPasswordChange} type="password" name="pass"/><br/>
 	
         <button onClick={this.login} id="loginButton">
           Login
@@ -57,7 +79,10 @@ class App extends React.Component {
 
         <button onClick={this.signUp} id="signupButton" >
           Sign Up
-			  </button>
+	</button>
+	
+	</form>
+	
       </div>
     );
 
