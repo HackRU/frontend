e28d915308894377c3bbc5a2f9a4747b7cd37f60
Fan Liu <fl273@assembly.cs.rutgers.edu>
2018-01-21 21:32:28 -0500

    eggs

--- react_app.js
+++ react_app.js
@@ -1,17 +1,67 @@
-mport React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, browserHistory } from 'react-router';
+import 'whatwg-fetch'
+
+class Callback extends React.Component {
+  login() {
+    fetch('https://m7cwj1fy7c.execute-api.us-west-2.amazonaws.com/test/authorize', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: 'email here',
+        password: 'password',
+      })
+    })
+  }
+
+  signUp() {
+    fetch('https://m7cwj1fy7c.execute-api.us-west-2.amazonaws.com/test/create', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: 'email here',
+        password: 'password',
+      })
+    })
+
+  }
+
+  mlh() {
+    window.location.href = "https://my.mlh.io/oauth/authorize?client_id=bab4ace712bb186d8866ff4776baf96b2c4e9c64d729fb7f88e87357e4badcba&redirect_uri=https://m7cwj1fy7c.execute-api.us-west-2.amazonaws.com/test/mlhcallback&response_type=code&scope=email+education+birthday";
+  }
+
+
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.login} id="loginButton">
+          Login
+	</button>
+
+        <button onClick={this.mlh} id="mlhButton">
+          MLH
+        </button>
+
+        <button onClick={this.signUp} id="signupButton" >
+          Sign Up
+	</button>
+      </div>
+    );
+
+  }
+
+
+}
 
 ReactDOM.render(
-    Username:<br>
-      <input type="text">
-        <br>
-          Password:<br>
-            <input type="text">
-              <br><br>
-
-   <button type="button">Login</button>
-   <button type="button">MLH</button>
-   <button type="button">Sign Up</button>
+  <Callback />,
+  document.getElementById('root')
 )
 
