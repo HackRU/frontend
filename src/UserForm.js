0914079f68a54a12dfae0836fe7ab55177af24fc
EC2 Default User <ec2-user@ip-172-31-18-2.us-west-2.compute.internal>
2018-02-07 02:07:41 -0500

    FAQ changes, errors--

--- src/UserForm.js
+++ src/UserForm.js
@@ -114,7 +114,7 @@ class UserForm extends React.Component {
         { this.state.user && 
             Object.keys(this.state.user)
               .map(key =>
-                 <div class="form-bit">
+                 <div className="form-bit">
                         <label>{key.replace(/_/g, ' ')}</label>
                         <input type="input" id={"input-" + key} value={this.state.user[key]} onChange={this.onChange}/><br/>
                  </div>
