963a72f9049172e0757a190d279ee78c1b76c9cb
thekabistro <kabir@kabirkuriyan.com>
2018-02-17 20:57:15 -0500

    Attempt to make a component...

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Col from './Col';
 import './App.css';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('register-root'));
+ReactDOM.render(<Col messageText="thisistext" />, document.getElementById('second-root'));
 registerServiceWorker();
