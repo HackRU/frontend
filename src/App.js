b6ef2cce89ee886e0dfd42f4c239f00f89c9eefb
thekabistro <kabir@kabirkuriyan.com>
2018-03-10 18:43:03 -0500

    Undo alerts

--- src/App.js
+++ src/App.js
@@ -159,7 +159,7 @@ class App extends React.Component {
 					</div>
 				</div>
 
-				<div className="form-group row my-5 alert alert-danger">
+				<div className="form-group row my-5 ">
           <h4>{this.state.errorMessage}</h4>
         </div>
 
