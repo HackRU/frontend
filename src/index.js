a7c696957361dac9659de09c0d767d338c26167e
Heman <hemangandhi@gmail.com>
2018-03-10 13:27:14 -0500

    login form functions

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Col from './Col';
+// import Col from './Col';
 import './App.css';
 import registerServiceWorker from './registerServiceWorker';
+import {CookiesProvider} from 'react-cookie';
 
-ReactDOM.render(<App />, document.getElementById('register-root'));
+ReactDOM.render(<CookiesProvider><App /></CookiesProvider>, document.getElementById('register-root'));
 // ReactDOM.render(<Col messageText="thisistext" />, document.getElementById('second-root'));
 registerServiceWorker();
