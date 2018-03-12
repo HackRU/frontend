20a653360b69e7350ca2d0797246f64cabf7597a
Heman <hemangandhi@gmail.com>
2018-03-11 22:42:14 -0400

    href and no HackHERS merde

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ class App extends React.Component {
         const { cookies } = this.props;//I don't get it.
         cookies.set('authdata', auth);
 
-        location.reload();
+        window.location.href = '/';
 
       }
     }
