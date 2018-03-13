3a7348cf8702ab47df1eff585c7919cb1ee9157c
thekabistro <kabir@kabirkuriyan.com>
2018-03-12 21:27:02 -0400

    Loading window!

--- src/modalerror.js
+++ src/modalerror.js
@@ -19,14 +19,14 @@ render() {
     <div>
 
     <div className="modal-header">
-      <h5 className="modal-title  font-modal" id="exampleModalLongTitle">Error.</h5 >
+      <h5 className="modal-title  font-modal" id="exampleModalLongTitle">Loading....</h5 >
       <button type="button" className="close" data-dismiss="modal" aria-label="Close">
         <span aria-hidden="true">&times;</span>
       </button>
     </div>
     <div className="modal-body">
       <div className="react-form font-modal">
-        Please try again.
+        If form does not load, please close this window and try again.
       </div>
 
     </div>
