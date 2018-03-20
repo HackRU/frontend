8ac009f2cdb25688dfda1c3e6b854b6f25a7d099
Heman <hemangandhi@gmail.com>
2018-03-19 23:16:44 -0400

    admin thingy works ish

--- src/Admin.js
+++ src/Admin.js
@@ -7,11 +7,50 @@ class Admin extends React.Component {
 constructor (props){
     super(props);
     this.state = {
-      messageText:'samele message',
+      user: props.user,
+      token: props.token
     };
+    this.doQuery = this.doQuery.bind(this);
+}
+
+doQuery(){
+  let grps = {};
+  document.querySelectorAll('.agg-filter')
+    .forEach(inp => {
+      if(!inp.checked) return;
+      let key = inp.getAttribute('id').replace('aggregate-', '');
+      grps[key] = '$' + key;
+  });
+
+  fetch('https://m7cwj1fy7c.execute-api.us-west-2.amazonaws.com/mlhtest/read', {
+    method: 'POST',
+    mode: 'cors',
+    credentials: 'omit',
+    headers: {
+      //'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      'email': this.state.user.email,
+      'token': this.state.token,
+      'query': [{'$group': {'_id': grps, 'count': {'$sum': 1}}}],
+      'aggregate': true
+    })
+  }).then(resp => resp.json())
+    .then(data => {
+      if(data.statusCode === 200)
+        this.setState({results: data.body});
+      else
+        this.setState({results: [{'_id': 'error', 'count': data.body}]})
+    });
+
 }
 
 render() {
+  if(!this.state.user || (!this.state.user.role.organizer && !this.state.user.role.director)){
+    return null;
+  }
+
   return (
 
     <div class="card row">
@@ -24,41 +63,35 @@ render() {
         </div>
 
       <h1 class="text-center">Admin Dashbaord</h1>
-      {
+      { this.state.user &&
         Object.keys(this.state.user).map(k =>
-          <input type="checkbox" id={"aggregate-" + k} />
-          <label htmlFor={"aggregate-" + k}>{k}</label>
-        );
+          <div className="form-group">
+            <input className="agg-filter" type="checkbox" id={"aggregate-" + k} />
+            <label htmlFor={"aggregate-" + k}>{k}</label>
+          </div>
+        )
       }
+
+          <button type="button" className="btn btn-primary custom-btn p-3  mx-1 my-3" onClick={this.doQuery}><h4 className="my-0">Query the DB</h4></button>,
 
         <table class="table table-dark">
           <thead class="thead-dark">
             <tr>
-              <th scope="col">#</th>
-              <th scope="col">First</th>
-              <th scope="col">Last</th>
-              <th scope="col">Handle</th>
+              <th scope="col">Category</th>
+              <th scope="col">Count</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-            </tr>
-            <tr>
-              <th scope="row">2</th>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-            </tr>
-            <tr>
-              <th scope="row">3</th>
-              <td>Larry</td>
-              <td>the Bird</td>
-              <td>@twitter</td>
-            </tr>
+          { this.state.results &&
+            this.state.results.map(count =>
+                <tr>
+                  <th scope="row">{Object.values(count._id)
+                    .map(v => '"' + v + '"')
+                    .join(" and ")}</th>
+                  <td>{count.count}</td>
+                </tr>
+            )
+          }
           </tbody>
         </table>
         </div>
