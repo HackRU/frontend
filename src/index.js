eae0d41f946b9cb2eae68bcb5842e07d299dd245
Kabir Kuriyan <thekabistro>
2018-04-17 21:48:09 -0400

    Annoucements Attemted

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import Col from './Col';
+import Slack from './Slack';
 import './App.css';
 import registerServiceWorker from './registerServiceWorker';
 import {CookiesProvider} from 'react-cookie';
 
 ReactDOM.render(<CookiesProvider><App /></CookiesProvider>, document.getElementById('register-root'));
+ReactDOM.render(<Slack></Slack>, document.getElementById('id-announcements'));
 // ReactDOM.render(<Col messageText="thisistext" />, document.getElementById('second-root'));
 registerServiceWorker();
