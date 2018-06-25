d0167e728fb701d276b1ea1ea2457f7f254c0553
Ez <ezrabl42@gmail.com>
2018-06-24 20:46:43 -0400

    README updated

--- src/index.js
+++ src/index.js
@@ -16,10 +16,26 @@ import {CookiesProvider} from 'react-cookie';
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(reduxThunk)));
 
+const Dashboard = () => (
+	<div>
+		<Provider store = {store}>
+			<CookiesProvider> 
+				<App />
+			</CookiesProvider>	
+			<Slack />
+			<Events />
+		</Provider>
+	</div>
+)
+//ReactDOM.render(<CookiesProvider><App /></CookiesProvider>, document.getElementById('register-root'));
+//ReactDOM.render(<Slack></Slack>, document.getElementById('announcements-list'));
+//ReactDOM.render(<Events></Events>, document.getElementById('upcoming-list'));
 
-ReactDOM.render(<CookiesProvider><App /></CookiesProvider>, document.getElementById('register-root'));
-ReactDOM.render(<Slack></Slack>, document.getElementById('announcements-list'));
-ReactDOM.render(<Events></Events>, document.getElementById('upcoming-list'));
+
 
 // ReactDOM.render(<Col messageText="thisistext" />, document.getElementById('second-root'));
+
+ReactDOM.render(<Dashboard />, document.getElementById('root'))
+
 registerServiceWorker();
+
