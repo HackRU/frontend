5473e0120df5d2dcc68cefb75cf6278f15b60521
Ez <ezrabl42@gmail.com>
2018-07-02 21:09:01 -0400

    new .eslintrc, contrib doc to follow

--- src/static/DashboardProfile.js
+++ src/static/DashboardProfile.js
@@ -8,15 +8,14 @@ const DashboardProfile = () => (
   <div className="col-xs-12 col-md-2" id="dashboard-profile">
     <div className="row">
       <div className="col-12 text-center">
-        <a href="http://hackru.org/"><img id="profile-logo" src={logoURL}/></a>
+        <a href="http://hackru.org/"><img id="profile-logo" alt="" src={logoURL}/></a>
       </div>
-      <div className="col-10 offset-1 text-center mt-4 mb-5" style="display:none;" id="qr-border">
-        <img className="image-divider" id="profile-qr" src={wheelURL}/>
+      <div className="col-10 offset-1 text-center mt-4 mb-5" id="qr-border" style={{display:'none'}}>
+        <img className="image-divider" id="profile-qr"  alt="" src={wheelURL}/>
       </div>
-      <div className="col-10 offset-1 text-left mb-5" id="register-sidebar">
-      </div>
+      <div className="col-10 offset-1 text-left mb-5" id="register-sidebar" />
     </div>
   </div>
-  );
+);
 
 export default DashboardProfile;
