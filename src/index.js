0ac23e3747fe33b84bbe5b34e97e60c3f1ef42bb
Ez <ezrabl42@gmail.com>
2018-07-07 17:34:59 -0400

    put noopener noreferrer on several refs

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ import { Provider } from 'react-redux';
 import reduxThunk from 'redux-thunk';
 
 
-import 'static/DashboardProfile.js'
+import DashboardProfile from 'static/DashboardProfile';
+import InfoSection from 'static/InfoSection';
+import ImportantLinks from 'static/ImportantLinks';
 
 import 'styles/index.css';
 import 'styles/App.css';
@@ -40,158 +42,89 @@ const Dashboard = () => (
     <Provider store={store}>		
       {/*BEGIN LEFT SIDE OF DASHBOARD*/}
       <DashboardProfile />
-			{/*END LEFT SIDE OF DASHBOARD*/}
-			{/*BEGIN RIGHT SIDE OF DASHBOARD*/}
-      <div class="col-xs-12 col-md-10 offset-md-2" id="dashboard-content">
-        <div class="row mb-5">
-          <div class="col-xs-12 col-md-5 mb-3">
+      {/*END LEFT SIDE OF DASHBOARD*/}
+      {/*BEGIN RIGHT SIDE OF DASHBOARD*/}
+      <div className="col-xs-12 col-md-10 offset-md-2" id="dashboard-content">
+        <div className="row mb-5"> {/*row mb-5 begin*/}
+          <div className="col-xs-12 col-md-5 mb-3">
             <div id="announcements-id">
-              <h2 class="content-section-title"><i class="fas fa-bullhorn fa-fw"></i> <span class="u-highlight">Announcements</span></h2>
-              <div class="content-section-desc" id="announcements-list">
+              <h2 className="content-section-title">
+                <i className="fas fa-bullhorn fa-fw" /> <span className="u-highlight">{'Announcements'}</span></h2>
+              <div className="content-section-desc" id="announcements-list">
                 <Slack /> {/*Slack component goes here*/}
               </div><span title=".announcements-list" HIDDEN/>
             </div><span title=".announcements-id" HIDDEN/>
           </div>
-          <div class="col-xs-12 col-md-4 mb-3">
-            <div class="content-section" id="upcoming-div">
-              <h2 class="content-section-title">
-              <i class="fas fa-calendar-alt fa-fw"></i> 
-              <span class="u-highlight">Upcoming</span></h2>
-              <div class="content-section-desc" id="upcoming-list">
+          <div className="col-xs-12 col-md-4 mb-3">
+            <div className="content-section" id="upcoming-div">
+              <h2 className="content-section-title">
+                <i className="fas fa-calendar-alt fa-fw" /> 
+                <span className="u-highlight">{'Upcoming'}</span></h2>
+              <div className="content-section-desc" id="upcoming-list">
                 <Events /> {/*Events component goes here*/}
               </div><span title=".upcoming-list" HIDDEN/>
-						</div>
+            </div>
           </div>
-          <div class="col-xs-12 col-md-3 mb-3">
+          <div className="col-xs-12 col-md-3 mb-3">
             <InfoSection />
-						<div class="content-section" id="info-div">
-            <h2 class="content-section-title"><i class="fas fa-info-circle fa-fw"></i> <span class="u-highlight">Info</span></h2>
-              <div class="content-section-desc" id="info-list">
-
-                <h5 class="content-title">Date</h5>
-                <p class="content-desc">
-                  April 21st-22nd
-                </p>
-                <h5 class="content-title">Location</h5>
-                <span class="font-weight-bold blue"><a class="blue" href="https://goo.gl/maps/a6gNk22V6px">Rutgers Athletic Center</a></span>
-                <p class="content-desc">
-                  83 Rockafeller Rd, <br>Piscataway Township, </br>NJ 08854
-                </p>
-                <h5 class="content-title">Devpost</h5>
-                <p class="content-desc">
-                  Click <a href="https://hackru-s18.devpost.com/">here</a> to view the devpost
-                </p>
-                <h5 class="content-title">Want to mentor?</h5>
-                <p class="content-desc">
-                  Click <a href="https://docs.google.com/forms/d/e/1FAIpQLSdGuoY6ATqMlOxOqoiP8FPyzgWMGg60gVzryvUrQBjV6T8h-w/viewform">here</a> to sign up
-                </p>
-                <h5 class="content-title">Questions?</h5>
-                <p class="content-desc">
-                Email us at <a href="mailto:info@hackru.org">info@hackru.org</a>
-                </p>
-                <h5 class="content-title">Emergency Contact</h5>
-                <p class="content-desc">
-                  <span class="font-weight-bold">RUPD</span><br>732-932-7211</br>
-                </p>
-
-
-              </div><span title=".info-list" HIDDEN/>
-            </div><span title=".info-div" HIDDEN/>
-					</div>
-								</div>
-
-
-				<div class="row mb-5">
-					<div class="col-xs-12 col-md-8 offset-md-2 mb-3">
-
-								<div id="register-root">
-								</div>
-								<div id="register-more">
-								</div>
-						</div>
-
-				</div> <!-- row -->
-
-				<div class="row mb-5">
-					<div class="col-xs-12 col-md-8 offset-md-2 mb-3">
-
-								<div id="announcements-id">
-									<h2 class="content-section-title"><i class="fas fa-video fa-fw"></i> <span class="u-highlight">Livestream</span></h2>
-									<div class="content-section-desc" id="livestream text-center">
-										<div id="all">
-    									<div class="sub">
-												<iframe src="https://player.twitch.tv/?channel=hack_ru" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620"></iframe>
-											</div>
-										</div>
-									<h5><a href="https://www.twitch.tv/hack_ru?tt_content=text_link&tt_medium=live_embed" style="display:block;" class="blue text-center">Watch live video from Hack_RU on www.twitch.tv</a></h5>
-
-								</div>
-							</div>
-						</div>
-					</div>
-
-
-				<div class="row mb-5">
-					<div class="col-xs-12 col-md-8 offset-md-2 mb-3">
-
-							<div class=" mt-2 mb-2" id="register-admin">
-
-							</div>
-						</div>
-
-				</div> <!-- row -->
-
-
-
-
-				<div class="row">
-
-
-					<div class="col-xs-12 col-md-2 offset-xs-0 offset-md-5">
-						<div class="row">
-
-							<div class="col-3 text-center">
-								<h5>
-									<a href="https://www.facebook.com/theHackRU/" class="color-two" target="_blank"><i class="fab fa-facebook-square fa-fw"></i></a>
-								</h5>
-							</div>
-
-							<div class="col-3 text-center">
-								<h5>
-									<a href="https://www.instagram.com/thehackru/" class="color-two" target="_blank"><i class="fab fa-instagram fa-fw"></i></a>
-								</h5>
-							</div>
-
-							<div class="col-3 text-center">
-								<h5>
-									<a href="https://medium.com/the-hackru" class="color-two" target="_blank"><i class="fab fa-medium fa-fw"></i></a>
-								</h5>
-							</div>
-
-							<div class="col-3 text-center">
-								<h5>
-									<a href="https://twitter.com/theHackRU" class="color-two" target="_blank"><i class="fab fa-twitter-square fa-fw"></i></a>
-								</h5>
-							</div>
-
-							<div class="col-12 text-center mt-3">
-								<p><a href="https://static.mlh.io/docs/mlh-code-of-conduct.pdf" class="color-two" target="_blank">MLH's Code of Conduct</a></p>
-								<p><a href="mailto:info@hackru.org" class="color-two" target="_blank" data-email="info@hackru.org">info@hackru.org</a></p>
-							</div>
-
-						</div>
-					</div>
-
-				</div>
-
-
-			</div>
+          </div> 
+        </div> {/*row mb-5 end*/}
+        <div className="row mb-5"> {/*row mb-5 begin*/}
+          <div className="col-xs-12 col-md-8 offset-md-2 mb-3">
+            <div id="register-root">
+              <CookiesProvider>
+                <App /> {/*App component goes here*/}
+              </CookiesProvider>
+            </div>
+            <div id="register-more" />
+          </div>
+        </div> {/*row mb-5 end*/}
+        <div className="row mb-5"> {/*row mb-5 begin*/}
+          <div className="col-xs-12 col-md-8 offset-md-2 mb-3">
+            <div id="announcements-id">
+              <h2 className="content-section-title"><i className="fas fa-video fa-fw" /> <span className="u-highlight">{'Livestream'}</span></h2>
+              <div className="content-section-desc" id="livestream text-center">
+                <div id="all">
+                  <div className="sub">
+                    <iframe title="livestream-iframe"
+                      src="https://player.twitch.tv/?channel=hack_ru" 
+                      frameBorder="0" 
+                      allowFullScreen="true" 
+                      scrolling="no" 
+                      height="378" 
+                      width="620"
+                    >
+                      {/*Twitch stream*/}
+                    </iframe>
+                  </div>
+                </div>
+                <h5>
+                  <a href="https://www.twitch.tv/hack_ru?tt_content=text_link&tt_medium=live_embed" 
+                    style={{display: 'block'}} 
+                    className="blue text-center"
+                  >
+                    {'Watch live video from Hack_RU on www.twitch.tv'} {/*Twitch Link*/} 
+                  </a>
+                </h5>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row mb-5"> {/*row mb-5 nested begin*/}
+          <div className="col-xs-12 col-md-8 offset-md-2 mb-3">
+            <div className=" mt-2 mb-2" id="register-admin" />
+          </div>
+        </div> {/*row mb-5 nested end*/}
+        <div className="row">
+          <div className="col-xs-12 col-md-2 offset-xs-0 offset-md-5">
+            <ImportantLinks />
+          </div>
+        </div>
+      </div>
     </Provider>
-	</div>
-
-	);
+  </div>
+);
 
 ReactDOM.render(<Dashboard />, document.getElementById('dashboard-full'));
-
 registerServiceWorker();
 
--- src/resources/resURLS.js
+++ src/resources/resURLS.js
@@ -1,8 +1,7 @@
-var resURLS.js = {
-	
-	logoURL: {process.env.PUBLIC_PATH} + 'img/logo-white.png',
-	wheelURL: {process.env.PUBLIC_PTH} + 'img/wheel.png'
+var resURLS = {
+  logoURL: process.env.PUBLIC_PATH + 'img/logo-white.png',
+  wheelURL: process.env.PUBLIC_PATH + 'img/wheel.png'
 
 };
 
-export {resURLS.js}
+export {resURLS};
