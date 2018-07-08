a197c9f6de6938442a4d450f8888700f1221e99c
Ez <Ezrabl42@gmail.com>
2018-07-08 16:53:07 -0400

    fuck

--- src/reducers/LoginManagement.js
+++ src/reducers/LoginManagement.js
@@ -1,8 +1,38 @@
 //LoginManagement.js
-import LOGIN_MNGMNT_ACTIONS from 'actions/ActionTypes';
+import LOGIN_MNGMNT from 'actions/ActionTypes';
 
 const initialState = {
   email: '',
   password: '',
+  link: '',
+  isLoggedIn: false, 
   errorMessage: ''
 };
+
+const LoginManagement = (state = initialState, action) => {
+  switch(action.type) {
+    case LOGIN_MNGMNT.CHANGE_EMAIL:
+      return {
+        ...state, 
+        email: action.email
+      };
+    case LOGIN_MNGMNT.CHANGE_PASSWORD:
+      return {
+        ...state, 
+        password: action.password
+      };
+    case LOGIN_MNGMNT.GOTO_USER_FORM:
+      return {
+      };
+    case LOGIN_MNGMNT.SET_ERROR:
+      return {
+        ...state, 
+        errorMessage: action.errorMessage,
+        link: action.link
+      }
+    default: 
+      return state;
+  }
+};
+
+export default LoginManagement;
