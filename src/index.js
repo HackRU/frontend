73aaca2179b9a4af028d185e9b8a31bee6f0d140
hemangandhi <hemangandhi@gmail.com>
2018-07-22 22:49:06 -0400

    purple

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ import registerServiceWorker from './registerServiceWorker';
 import {CookiesProvider} from 'react-cookie';
 
 ReactDOM.render(<CookiesProvider><App /></CookiesProvider>, document.getElementById('register-root'));
-ReactDOM.render(<Slack></Slack>, document.getElementById('announcements-list'));
-ReactDOM.render(<Events></Events>, document.getElementById('upcoming-list'));
+// ReactDOM.render(<Slack></Slack>, document.getElementById('announcements-list'));
+// ReactDOM.render(<Events></Events>, document.getElementById('upcoming-list'));
 
 // ReactDOM.render(<Col messageText="thisistext" />, document.getElementById('second-root'));
 registerServiceWorker();
