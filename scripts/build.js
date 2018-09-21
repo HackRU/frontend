14c8b50b7fccebb4ce0bf64dff871ae997103e3c
Heman Gandhi <hemangandhi@gmail.com>
2018-09-21 18:10:31 -0400

    test

--- scripts/build.js
+++ scripts/build.js
@@ -2,7 +2,12 @@
 
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'production';
-process.env.NODE_ENV = 'production';
+process.env.NODE_ENV = 'development';
+
+let webpackConfPath = '../config/webpack.config.prod'
+if(process.env.NODE_ENV === 'development'){
+  webpackConfPath = '../config/webpack.config.dev'
+}
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
@@ -18,7 +23,7 @@ const path = require('path');
 const chalk = require('chalk');
 const fs = require('fs-extra');
 const webpack = require('webpack');
-const config = require('../config/webpack.config.prod');
+const config = require(webpackConfPath);
 const paths = require('../config/paths');
 const checkRequiredFiles = require('react-dev-utils/checkRequiredFiles');
 const formatWebpackMessages = require('react-dev-utils/formatWebpackMessages');
