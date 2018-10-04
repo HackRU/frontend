4e11ae1db9b67011d5908c190cab5487bfcc5e21
Ryan Goldstein <ryanisaacg@gmail.com>
2018-10-04 19:43:52 -0400

    Fix bug in SlackMessage rendering

--- src/dumb_components/SlackMessage.js
+++ src/dumb_components/SlackMessage.js
@@ -1,21 +1,21 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 
-class SlackMessage extends Component {
-  render = () => (
-    <Fragment>
-      <h5 className="content-title"> {this.props.contents} </h5>
-      <p className="content-desc mb-3">
-        {`Posted on ${this.props.date} at ${this.props.time}`}
-      </p>
-    </Fragment>
-  )
-}
+const SlackMessage = ({ message: { date, text, time }}) => (
+  <Fragment>
+    <h5 className="content-title"> {text} </h5>
+    <p className="content-desc mb-3">
+      {`Posted on ${date} at ${time}`}
+    </p>
+  </Fragment>
+);
 
 SlackMessage.propTypes = {
-  contents: PropTypes.string.isRequired,
-  date: PropTypes.instanceOf(Date).isRequired,
-  time: PropTypes.instanceOf(Date).isRequired,
+  message: PropTypes.shape({
+    date: PropTypes.instanceOf(Date).isRequired,
+    time: PropTypes.instanceOf(Date).isRequired,
+    text: PropTypes.string.isRequired,
+  }).isRequired
 };
 
 export default SlackMessage;
