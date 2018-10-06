5dc2c256ed279b7c87040bb6c7f88ea3b972abeb
Kabir <kabir@kabirkuriyan.com>
2018-10-06 15:51:33 -0400

    Styling fixes

--- src/dumb_components/DetailLinks.js
+++ src/dumb_components/DetailLinks.js
@@ -1,28 +1,26 @@
 import React from 'react';
 
 const Detail = ({ iconClass, text }) => (
-  <div className="col-12 col-md-4 offset-0 offset-md-2 col-lg-2 offset-lg-1 text-center">
+  <div className="col-xs-1 col-md-2 text-center ">
     <h1>
       <i className={`${iconClass} theme-red blue`} aria-hidden="true" />
     </h1>
-    <h6 className="about-icon-desc text-uppercase bold-text">
+    <h6 className="about-icon-desc text-uppercase bold-text blue">
       { text }
     </h6>
   </div>
 );
 
 const DetailLinks = () => (
-  <div className="row min-full-height color-two">
-    <div className="row-content-box col-12 theme-purple blue">
-      <div className="row" style={{color: 'red'}}>
-        <Detail iconClass="fa fa-file" text="Waiver" />
-        <Detail iconClass="fa fa-code" text="DevPost" />
-        <Detail iconClass="fab fa-slack-hash" text="Slack" />
-        <Detail iconClass="fa fa-question" text="HelpQ" />
-        <Detail iconClass="fa fa-utensils" text="Food Menu" />
-      </div>
-    </div>
+
+  <div className="row offset-sm-0 offset-md-1" style={{color: 'red'}}>
+    <Detail iconClass="fa fa-file" text="Waiver" />
+    <Detail iconClass="fa fa-code" text="DevPost" />
+    <Detail iconClass="fab fa-slack-hash" text="Slack" />
+    <Detail iconClass="fa fa-question" text="HelpQ" />
+    <Detail iconClass="fa fa-utensils" text="Food Menu" />
   </div>
+
 );
 
 export default DetailLinks;
