6493021286a68606858e83ccf826b9c94c426bac
Ryan Goldstein <ryanisaacg@gmail.com>
2018-10-06 15:51:44 -0400

    Add the links

--- src/dumb_components/DetailLinks.js
+++ src/dumb_components/DetailLinks.js
@@ -1,25 +1,28 @@
 import React from 'react';
+import resURLS from 'resources/resURLS';
 
-const Detail = ({ iconClass, text }) => (
-  <div className="col-12 col-md-4 offset-0 offset-md-2 col-lg-2 offset-lg-1 text-center">
-    <h1>
-      <i className={`${iconClass} theme-red blue`} aria-hidden="true" />
-    </h1>
-    <h6 className="about-icon-desc text-uppercase bold-text">
-      { text }
-    </h6>
-  </div>
+const Detail = ({ iconClass, link, text }) => (
+  <a className="blue" href={link} target="_blank" rel="noopener noreferrer">
+    <div className="col-12 col-md-4 offset-0 offset-md-2 col-lg-2 offset-lg-1 text-center">
+      <h1>
+        <i className={`${iconClass} theme-red blue`} aria-hidden="true" />
+      </h1>
+      <h6 className="about-icon-desc text-uppercase bold-text">
+        { text }
+      </h6>
+    </div>
+  </a>
 );
 
 const DetailLinks = () => (
   <div className="row min-full-height color-two">
     <div className="row-content-box col-12 theme-purple blue">
       <div className="row" style={{color: 'red'}}>
-        <Detail iconClass="fa fa-file" text="Waiver" />
-        <Detail iconClass="fa fa-code" text="DevPost" />
-        <Detail iconClass="fab fa-slack-hash" text="Slack" />
-        <Detail iconClass="fa fa-question" text="HelpQ" />
-        <Detail iconClass="fa fa-utensils" text="Food Menu" />
+        <Detail iconClass="fa fa-file" link={resURLS.waiver} text="Waiver" />
+        <Detail iconClass="fa fa-code" link={resURLS.devpost} text="DevPost" />
+        <Detail iconClass="fab fa-slack-hash" link={resURLS.slack} text="Slack" />
+        <Detail iconClass="fa fa-question" link={resURLS.helpq} text="HelpQ" />
+        <Detail iconClass="fa fa-utensils" link={resURLS.menu} text="Food Menu" />
       </div>
     </div>
   </div>
