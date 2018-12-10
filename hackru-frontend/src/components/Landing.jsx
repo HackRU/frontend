6b0da7dbba2975512a9b428b61b90a9b7ebdf002
Shivan Modha <shivanmodha@gmail.com>
2018-12-09 21:07:49 -0500

    Before reactstrap

--- hackru-frontend/src/components/Landing.jsx
+++ hackru-frontend/src/components/Landing.jsx
@@ -1,13 +1,29 @@
+/**
+ * @author Shivan Modha
+ * @description The website homepage, which will be the first thing that new users see
+ * @version 0.0.1
+ * Created 12/09/18
+ */
+/***************************************************************IMPORTS***************************************************************/
 import React, { Component } from 'react';
+/***************************************************************IMPORTS***************************************************************/
 
-class App extends Component {
+/*****************************************************************APP*****************************************************************/
+/**
+ * This is the first thing that users will see. It will include information about the upcoming hackathon, an about section, and other
+ * things that need to be shown on the homepage
+ */
+class LandingPage extends Component {
     render() {
         return (
-            <div className="App">
-                Shivan Modha
+            <div>d
+                Landing
             </div>
         );
     }
 }
+/*****************************************************************APP*****************************************************************/
 
-export default App;
+/***************************************************************EXPORTS***************************************************************/
+export default LandingPage;
+/***************************************************************EXPORTS***************************************************************/
---
+++ hackru-frontend/src/components/Schedule.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Schedule extends Component {
+    render() {
+        return (
+            <div className="App">
+                Schedule
+            </div>
+        );
+    }
+}
+
+export default Schedule;
