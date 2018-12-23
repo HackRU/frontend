1e3b4c58f8cdcfa96f417c75e8ed054a4b0e4e55
Kabir <kabir@kabirkuriyan.com>
2018-12-23 14:11:32 -0500

    Responsive Stlying & Footer

--- hackru-frontend/src/components/Landing/Landing.jsx
+++ hackru-frontend/src/components/Landing/Landing.jsx
@@ -9,6 +9,7 @@ import React, { Component } from "react";
 import { Container, Row, Col } from "reactstrap";
 import Navigation from "./Navigation";
 import Home from "./Sections/Home";
+import Footer from "./Sections/Footer"
 import HomePOC from "./Sections/HomePOC";
 import Aliens from "./Aliens";
 import { ParallaxProvider } from "react-scroll-parallax";
@@ -94,34 +95,11 @@ class LandingPage extends Component {
                     {rows}
                     </ParallaxProvider>
                     <div>
-                        <Row style={{ height: 250, overflowY: "hidden", marginTop: -100 }}>
-                            <div className="bg-gradient-right skew-right" style={{ marginTop: 100, padding: 0, left: 0, zIndex: "15", height: "100%", width: "100%" }}>
-                                <div className="skew-left" style={{ padding: 85, textAlign: "center" }}>
-                                    <Container fluid>
-                                        <Row>
-                                            <Col>
-                                                <a href="https://mlh.io/" target="_blank" rel="noopener noreferrer">
-                                                    <img style={{ width: 150, marginTop: -25 }} src="https://static.mlh.io/brand-assets/logo/official/mlh-logo-black.svg" alt="MLH logo" />
-                                                </a>
-                                                <a href="http://usacs.rutgers.edu/" target="_blank" rel="noopener noreferrer">
-                                                    <img style={{ width: 100, marginTop: -40, marginLeft: 25 }} src="./assets/icons/usacs-logo-black.svg" alt="MLH logo" />
-                                                </a>
-                                            </Col>
-                                            <Col>
-                                                <a style={{ color: "white", marginLeft: 10, marginRight: 10 }} href="mailto:info@hackru.org" class="social-links" target="_blank" rel="noopener noreferrer"><Icon size="2x" name="envelope" /></a>
-                                                <a style={{ color: "white", marginLeft: 10, marginRight: 10 }} href="https://www.facebook.com/theHackRU/" class="social-links" target="_blank" rel="noopener noreferrer"><Icon size="2x" name="facebook-square" /></a>
-                                                <a style={{ color: "white", marginLeft: 10, marginRight: 10 }} href="https://www.instagram.com/thehackru/" class="social-links" target="_blank" rel="noopener noreferrer"><Icon size="2x" name="instagram" /></a>
-                                                <a style={{ color: "white", marginLeft: 10, marginRight: 10 }} href="https://medium.com/the-hackru" class="social-links" target="_blank" rel="noopener noreferrer"><Icon size="2x" name="medium" /></a>
-                                                <a style={{ color: "white", marginLeft: 10, marginRight: 10 }} href="https://twitter.com/theHackRU" class="social-links" target="_blank" rel="noopener noreferrer"><Icon size="2x" name="twitter-square" /></a>
-                                            </Col>
-                                            <Col>
-                                                <a style={{ color: "white" }} href="https://static.mlh.io/docs/mlh-code-of-conduct.pdf" target="_blank" rel="noopener noreferrer">MLH's Code of Conduct</a>
-                                            </Col>
-                                        </Row>
-                                    </Container>
-                                </div>
-                            </div>
-                        </Row>
+                       
+                                <Footer />
+                            
+                       
+                        
                     </div>
                 </Container>
             );
--- hackru-frontend/src/components/Landing/Sections/Sponsors.jsx
+++ hackru-frontend/src/components/Landing/Sections/Sponsors.jsx
@@ -16,7 +16,7 @@ class Sponsors extends Component {
     render() {
         return (
             <div>
-                <h1 className="display-1 theme-font mb-3">Sponsorship</h1>
+                <h1 className="display-4 theme-font mb-3">Sponsorship</h1>
                 <p className=""> Our Spring 2019 Sponsors list is coming soon.  In the meantime, if you are interested in sponsoring HackRU, please email <a href="emailto:sponsorship@hackru.org">sponsorship@hackru.org</a>.</p>
             </div>
         )
