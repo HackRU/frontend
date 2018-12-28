be7481c5d455183ed95f315debee001510af1a5d
Shivan Modha <shivanmodha@gmail.com>
2018-12-28 18:56:59 -0500

    Fixed key error

--- hackru-frontend/src/components/Landing/Sections/FaqsCollapse.jsx
+++ hackru-frontend/src/components/Landing/Sections/FaqsCollapse.jsx
@@ -3,31 +3,31 @@ import { Collapse } from 'reactstrap';
 import { Icon } from "react-fa";
 
 class FaqsCollapse extends React.Component {
-  constructor(props) {
-    super(props);
-    
-    this.toggle = this.toggle.bind(this);
-    this.state = {collapse: false};
-  }
-  
-  toggle() {
-    this.setState({ collapse: !this.state.collapse });
-  }
-  
-  render() {
-    const cat = this.props.cat;
+    constructor(props) {
+        super(props);
+        this.toggle = this.toggle.bind(this);
+    }
+    componentWillMount() {
+        this.setState({
+            collapse: false
+        });
+    }
+    toggle() {
+        this.setState({ collapse: !this.state.collapse });
+    }
+    render() {
+        let cat = this.props.cat;
 
-    return (
-        <div>
-          <strong><h3 className="theme-font" onClick={this.toggle}>
-            {cat.title}
-            <Icon className="pull-right" name={(this.state.collapse) ? ("chevron-up") : ("chevron-down")} />
-          </h3></strong>
-          <hr />
-          <Collapse className="mb-3" isOpen={this.state.collapse}>{cat.text}</Collapse>
-        </div>
-    );
-  }
+        return (
+            <div>
+                <strong>
+                    <h3 className="theme-font" onClick={this.toggle}>{cat.title} <Icon className="pull-right" name={(this.state.collapse) ? ("chevron-up") : ("chevron-down")} /></h3>
+                </strong>
+                <hr />
+                <Collapse className="mb-3" isOpen={this.state.collapse}>{cat.text}</Collapse>
+            </div>
+        );
+    }
 }
 
 export default FaqsCollapse
