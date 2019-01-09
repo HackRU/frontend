bb25e84c502c004c8df2095850d8ac5ab17df22a
Ryan G <ryanisaacg@users.noreply.github.com>
2019-01-09 10:46:05 -0500

    Add support for travel reimbursements (#146)
    * Add support for travel reimbursements
    
    * Fix default open / closed for the user form section
    
    * Move the disclaimer above the save button
    
    * Move the maps API to the bottom of the page
    
    * Add the ability to pass props through Section to its inner div
    
    * Add a larger margin bottom to the travel section so it doesn't clip the edge of the page
    
    * Fix a warning with setting the button class
    
    * Remove the secret from the repository
    
    Now secrets needed from within JS code can be built into the source
    bundle.

---
+++ src/components/Dashboard/Section.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { Collapse, ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText } from 'reactstrap';
+import { Icon } from "react-fa";
+import { theme } from "../../Defaults";
+
+// A collapsable section on the dashboard
+// It takes four props, 'children', 'isOpen' (if it is initially expanded), 'title', and 'subtitle'
+class Section extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isOpen: props.isOpen
+        }
+    }
+
+    render() {
+        const { children, title, subtitle, isOpen, ...rest } = this.props;
+        return (
+            <div
+                style={{ width: "100%", textAlign: "left", marginBottom: 25 }}
+                {...rest}
+            >
+                <ListGroup>
+                    <ListGroupItem
+                        action 
+                        tag="button"
+                        href="#"
+                        style={{ background: theme.primary[0] + "2F", color: theme.primary[1] + "FF", borderRadius: 0 }}
+                        onClick={(e) => 
+                            this.setState({ isOpen: !this.state.isOpen })
+                        }
+                    >
+                        <ListGroupItemHeading>
+                            { title }
+                        </ListGroupItemHeading>
+                        <ListGroupItemText>
+                            { subtitle }
+                        </ListGroupItemText>
+                        <Icon style={{ position: "absolute", right: 25, top: 25 }} name={(this.state.isOpen) ? ("chevron-up") : ("chevron-down")} />
+                    </ListGroupItem>
+                    <Collapse isOpen={this.state.isOpen}>
+                        <ListGroupItem style={{ background: theme.primary[1] + "1F", borderRadius: 0 }}>
+                            { children }
+                        </ListGroupItem>
+                    </Collapse>
+                </ListGroup>
+            </div>
+        )
+    }
+
+}
+
+export default Section
