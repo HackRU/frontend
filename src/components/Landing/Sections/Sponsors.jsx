fdc534c0622981f0fa5565592c6f8d1747e658c6
DhruvilPatel <atdhruvilpatel@gmail.com>
2019-01-15 19:03:41 -0500

    metadata and hyperlink color issues fixed

--- src/components/Landing/Sections/Sponsors.jsx
+++ src/components/Landing/Sections/Sponsors.jsx
@@ -17,7 +17,7 @@ class Sponsors extends Component {
         return (
             <div>
                 <h1 className="display-4 theme-font mb-3">Sponsorship</h1>
-                <p> Our Spring 2019 Sponsors list is coming soon.  In the meantime, if you are interested in sponsoring HackRU, please email <a href="emailto:sponsorship@hackru.org">sponsorship@hackru.org</a>.</p>
+                <p> Our Spring 2019 Sponsors list is coming soon.  In the meantime, if you are interested in sponsoring HackRU, please email <a href="emailto:sponsorship@hackru.org" style={{color: "#354a5f", fontWeight: 500}}>sponsorship@hackru.org</a>.</p>
             </div>
         )
     }
