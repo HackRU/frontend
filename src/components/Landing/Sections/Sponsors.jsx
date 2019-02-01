d93072dbec031f67eba51d077e64e2ec9e120b70
Shivan Modha <shivanmodha@gmail.com>
2019-01-31 19:59:25 -0500

    Added sponsors images

--- src/components/Landing/Sections/Sponsors.jsx
+++ src/components/Landing/Sections/Sponsors.jsx
@@ -7,7 +7,7 @@ class Sponsors extends Component {
     render() {
         return (
             <div>
-                <h1 className="display-4 theme-font mb-3">Sponsorship</h1>
+                <h1 className="display-4 theme-font mb-3">Sponsors</h1>
                 <p> Our Spring 2019 Sponsors list is coming soon.  In the meantime, if you are interested in sponsoring HackRU, please email <a href="emailto:sponsorship@hackru.org">sponsorship@hackru.org</a>.</p>
             </div>
         )
