5dcf8747fdb9d9c0f4bb6d13dc77d6dfc1b7a322
Kabir <kabir@kabirkuriyan.com>
2019-02-04 21:14:02 -0500

    Add links

--- src/components/Landing/Sections/Sponsors/SponsorItem.jsx
+++ src/components/Landing/Sections/Sponsors/SponsorItem.jsx
@@ -5,7 +5,7 @@ class SponsorItem extends Component {
     render() {
         return (
             <div style={{ position: "relative", float: "none",  maxWidth: this.props.size.width, maxHeight: this.props.size.height }} className="d-flex align-items-center p-4" >
-                <img alt={this.props.name} className="sponsor-item-image" src={this.props.image}/>
+                <a href={this.props.href}><img alt={this.props.name} className="sponsor-item-image" src={this.props.image}/></a>
             </div>
         );
     }
