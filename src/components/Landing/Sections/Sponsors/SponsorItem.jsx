ae961e27faa5f1861a8bb29a428900a458807a16
Shivan Modha <shivanmodha@gmail.com>
2019-02-08 17:07:33 -0500

    (re)sponsors - responsive sponsors (#164)
    * Added sponsors images
    
    * Sponsor theme colors
    
    * Added sponsor containers/items
    
    * sponsor overlays
    
    * Background color on sponsorship section
    
    * Remove overlay
    
    * Justify center on two lines
    
    * Add links
    
    * Move sponsor logos to external S3 location

---
+++ src/components/Landing/Sections/Sponsors/SponsorItem.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { defaults } from "../../../../Defaults.js";
+
+class SponsorItem extends Component {
+    render() {
+        return (
+            <div style={{ position: "relative", float: "none",  maxWidth: this.props.size.width, maxHeight: this.props.size.height }} className="d-flex align-items-center p-4" >
+                <a href={this.props.href}><img alt={this.props.name} className="sponsor-item-image" src={defaults.sponsorshipLogos + "logos/" + this.props.image}/></a>
+            </div>
+        );
+    }
+}
+export default SponsorItem;
