e1451a246f963313f6a9241bfe8f3709a5ec8746
Shivan Modha <shivanmodha@gmail.com>
2019-02-08 16:53:11 -0500

    Updated Magic Links (#165)

--- src/components/Dashboard/ProfileMessage.jsx
+++ src/components/Dashboard/ProfileMessage.jsx
@@ -1,14 +1,13 @@
 import React from "react";
-import { UncontrolledAlert } from 'reactstrap';
+import { UncontrolledAlert } from "reactstrap";
 /**
  * Renders an alert based on the message
  * @param {String} message The displayed message 
  */
 const ProfileMessage = ({ message }) => (
-    message && <UncontrolledAlert
-            color={message.success ? 'success' : 'danger'}
-            style={{ background: "rgba(0, 255, 0, 0.25)", border: "none", color: "white" }} >
-        { message.value }
-    </UncontrolledAlert>
+    message &&
+        <UncontrolledAlert color={message.color} className={`profile-alert profile-alert-${message.color}`}>
+            {message.value}
+        </UncontrolledAlert>
 )
 export default ProfileMessage
