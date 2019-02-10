5a7392bf2cca2bb2bf732194a1a2d8eacaff1d60
Kabir <kabir@kabirkuriyan.com>
2019-02-10 17:46:50 -0500

    Add sponsor section

--- src/components/Landing/Sections/Sponsors/SponsorItem.jsx
+++ src/components/Landing/Sections/Sponsors/SponsorItem.jsx
@@ -5,7 +5,7 @@ class SponsorItem extends Component {
     render() {
         return (
             <div style={{ position: "relative", float: "none",  maxWidth: this.props.size.width, maxHeight: this.props.size.height }} className="d-flex align-items-center p-4" >
-                <a href={this.props.href}><img alt={this.props.name} className="sponsor-item-image" src={defaults.sponsorshipLogos + "logos/" + this.props.image}/></a>
+                <a href={this.props.href}><img alt={this.props.name} className="sponsor-item-image" src={this.props.baseURL + "logos/" + this.props.image}/></a>
             </div>
         );
     }
