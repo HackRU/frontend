d80e9a43e969801b051dcf91bb79b4bd1ba643b0
Ryan Goldstein <ryanisaacg@gmail.com>
2019-03-09 10:23:38 -0500

    Fix QR display

--- src/components/Dashboard/QR.jsx
+++ src/components/Dashboard/QR.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const QR = ({ data, status }) => (
-    (status === 'confirmed' || status === 'waitlisted' || status ==='coming' || status ==='registered' || true) && data && data.body && <div>
+    (status === 'confirmed' || status === 'waitlisted' || status ==='coming' || status ==='registered') && data && data.body && <div>
         <img src={data.body} className="qr" alt="User QR Code" />
     </div>
 )
