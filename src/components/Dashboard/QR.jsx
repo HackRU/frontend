fa006d07b98ed192f089a70e4f8ae91a95dd4180
Shivan Modha <shivanmodha@gmail.com>
2019-03-09 12:14:37 -0500

    Removed registered

--- src/components/Dashboard/QR.jsx
+++ src/components/Dashboard/QR.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const QR = ({ data, status }) => (
-    (status === 'confirmed' || status === 'waitlist' || status ==='coming' || status ==='registered') && data && data.body && <div>
+    (status === 'confirmed' || status === 'waitlist' || status ==='coming') && data && data.body && <div>
         <img src={data.body} className="qr" alt="User QR Code" />
     </div>
 )
