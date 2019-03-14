25a849e5a0c862f801f2ddc1f6e730b37ec44fad
Shivan Modha <shivanmodha@gmail.com>
2019-03-13 20:30:41 -0400

    Added to bitmap

--- src/components/AnimatedGrid.jsx
+++ src/components/AnimatedGrid.jsx
@@ -1,6 +1,179 @@
 import React, { Component } from "react";
 import anime from "animejs";
-class Countdown extends Component {
+import { theme } from "../Defaults";
+const N = "rgba(0, 0, 0, 0)";
+const C = theme.primary[0];
+const BITMAP = {
+    "0": [
+        [C, C, C],
+        [C, N, C],
+        [C, N, C],
+        [C, N, C],
+        [C, C, C]
+    ],
+    "1": [
+        [N, C, N],
+        [N, C, N],
+        [N, C, N],
+        [N, C, N],
+        [N, C, N]
+    ],
+    "2": [
+        [C, C, C],
+        [N, N, C],
+        [C, C, C],
+        [C, N, N],
+        [C, C, C]
+    ],
+    "3": [
+        [C, C, C],
+        [N, N, C],
+        [C, C, C],
+        [N, N, C],
+        [C, C, C]
+    ],
+    "4": [
+        [C, N, C],
+        [C, N, C],
+        [C, C, C],
+        [N, N, C],
+        [N, N, C]
+    ],
+    "5": [
+        [C, C, C],
+        [C, N, N],
+        [C, C, C],
+        [N, N, C],
+        [C, C, C]
+    ],
+    "6": [
+        [C, C, C],
+        [C, N, N],
+        [C, C, C],
+        [C, N, C],
+        [C, C, C]
+    ],
+    "7": [
+        [C, C, C],
+        [N, N, C],
+        [N, N, C],
+        [N, N, C],
+        [N, N, C]
+    ],
+    "8": [
+        [C, C, C],
+        [C, N, C],
+        [C, C, C],
+        [C, N, C],
+        [C, C, C]
+    ],
+    "9": [
+        [C, C, C],
+        [C, N, C],
+        [C, C, C],
+        [N, N, C],
+        [C, C, C]
+    ],
+    ":": [
+        [N, N, N],
+        [N, C, N],
+        [N, N, N],
+        [N, C, N],
+        [N, N, N]
+    ],
+    "H": [
+        [C, N, C],
+        [C, N, C],
+        [C, C, C],
+        [C, N, C],
+        [C, N, C]
+    ],
+    "A": [
+        [C, C, C],
+        [C, N, C],
+        [C, C, C],
+        [C, N, C],
+        [C, N, C]
+    ],
+    "C": [
+        [C, C, C],
+        [C, N, N],
+        [C, N, N],
+        [C, N, N],
+        [C, C, C]
+    ],
+    "K": [
+        [C, N, C],
+        [C, N, C],
+        [C, C, N],
+        [C, N, C],
+        [C, N, C]
+    ],
+    " ": [
+        [N],
+        [N],
+        [N],
+        [N],
+        [N]
+    ],
+    "R": [
+        [C, C, C],
+        [C, N, C],
+        [C, C, N],
+        [C, N, C],
+        [C, N, C]
+    ],
+    "U": [
+        [C, N, C],
+        [C, N, C],
+        [C, N, C],
+        [C, N, C],
+        [C, C, C]
+    ],
+    "W": [
+        [C, N, C, N, C],
+        [C, N, C, N, C],
+        [C, N, C, N, C],
+        [C, N, C, N, C],
+        [N, C, N, C, N]
+    ],
+    "I": [
+        [C, C, C],
+        [N, C, N],
+        [N, C, N],
+        [N, C, N],
+        [C, C, C]
+    ],
+    "L": [
+        [C, N, N],
+        [C, N, N],
+        [C, N, N],
+        [C, N, N],
+        [C, C, C]
+    ],
+    "T": [
+        [C, C, C],
+        [N, C, N],
+        [N, C, N],
+        [N, C, N],
+        [N, C, N]
+    ],
+    "E": [
+        [C, C, C],
+        [C, N, N],
+        [C, C, N],
+        [C, N, N],
+        [C, C, C]
+    ],
+    "N": [
+        [C, N, N, C],
+        [C, C, N, C],
+        [C, C, C, C],
+        [C, N, C, C],
+        [C, N, N, C]
+    ],
+}
+class AnimatedGrid extends Component {
     constructor() {
         super();
         this.animate = this.animate.bind(this);
@@ -53,4 +226,38 @@ class Countdown extends Component {
         )
     }
 }
-export default Countdown
+class TextGrid extends Component {
+    constructor() {
+        super();
+        this.generateTemplate = this.generateTemplate.bind(this);
+    }
+    componentWillMount() {
+        this.setState({
+            template: this.generateTemplate(this.props.text)
+        });
+    }
+    generateTemplate(str) {
+        let template = [];
+        for (let i = 0; i < 10; i++) {
+            template.push([]);
+        }
+        for (let i = 0; i < str.length; i++) {
+            let BIT = BITMAP[str.charAt(i)];
+            for (let j = 0; j < BIT.length; j++) {
+                template[j].push(...BIT[j]);
+                template[j].push(N);
+            }
+        }
+        return template;
+    }
+    render() {
+        return (
+            <AnimatedGrid padding={0} ref="grid" width={500} height={500} rows={10} cols={70} template={this.state.template} />
+        );
+    }
+
+}
+export default AnimatedGrid
+export {
+    BITMAP, N, C, TextGrid
+}
--- src/components/Landing/Sections/Freeze.jsx
+++ src/components/Landing/Sections/Freeze.jsx
@@ -1,15 +1,14 @@
 import React, { Component } from "react";
 import { Container, Row, Col } from "reactstrap";
+import { TextGrid } from "../../AnimatedGrid";
 class Freeze extends Component {
     render() {
         return (
             <Container fluid id="landing-section" style={{ zIndex: 3, width: "100%", minHeight: "100vh", textAlign: "center", userSelect: "none" }}>
                 <Row style={{ minWidth: "100%", minHeight: "100vh" }} className="d-flex align-items-center">
-                    <Col xs={2}></Col>
-                    <Col xs={8} style={{ display: "block" }}>
-                        
-                    </Col>
-                    <Col xs={2}></Col>
+                    <div style={{ display: "block", position: "absolute", top: window.innerHeight / 2, left: (window.innerWidth / 2) - 325 }}>
+                        <TextGrid text={"HACKRU WILL RETURN"} />
+                    </div>
                 </Row>
             </Container>
         )
