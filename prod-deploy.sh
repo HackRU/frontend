c750765d58731701b61f8ee021ff581141c8ddb6
Kabir Kuriyan <thekabistro@users.noreply.github.com>
2019-06-08 10:15:39 -0400

    Changing bucket names to be more understandable (#188)

--- prod-deploy.sh
+++ prod-deploy.sh
@@ -4,6 +4,6 @@
 
 npm run-script build
 #empty bucket, then upload
-aws s3 rm s3://hackru-deploy --recursive
-aws s3 cp --recursive build s3://hackru-deploy --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+aws s3 rm s3://hackru-frontend-prod --recursive
+aws s3 cp --recursive build s3://hackru-frontend-prod --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
 aws cloudfront create-invalidation --distribution-id "E1YIAV5VULIMY7"  --paths "/*"
