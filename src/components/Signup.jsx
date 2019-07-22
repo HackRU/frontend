4c87955a71a6b47290e970d89fd6041a150d6b0b
Ryan G <ryanisaacg@users.noreply.github.com>
2019-07-22 15:36:59 -0700

    Refactor the auth flow components to be DRY (#196)
    All the auth components have essentially the same structure, so a
    reusable 'AuthForm' can be created that handles that structure.

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
-import { Container, Col, Input, InputGroup, InputGroupAddon, Form, FormGroup, Button, FormText, Alert } from "reactstrap";
-import { theme } from "../Defaults";
+import { Col, Input, InputGroup, InputGroupAddon, FormGroup, Button, FormText } from "reactstrap";
 import { Icon } from "react-fa";
 import { Link } from "react-router-dom";
-import { RingLoader } from "react-spinners";
 import { Redirect } from "react-router-dom";
+import AuthForm from "../library/AuthForm";
 
 /**
  * Signup application for "/signup"
@@ -17,108 +16,84 @@ class SignUpPage extends Component {
             errors: ""
         });
     }
+
     render() {
         // Check if the user is already logged in
-        if (this.props.profile.isLoggedIn) {
+        if (this.props.profile.isLoggedIn || this.state.done) {
             return (<Redirect to="/dashboard" />);
         }
-        let innerText = "Join us at HackRU!";
-        let innerForm = (
-            <div>
-                <FormGroup row>
-                    <Col xs={6} style={{ margin: 0, paddingLeft: 0, paddingRight: 7 }}>
-                        <Input required id="first" type="text" placeholder="first name" />
-                    </Col>
-                    <Col xs={6} style={{ margin: 0, paddingRight: 0, paddingLeft: 7 }}>
-                        <Input required id="last" type="text" placeholder="last name" />
-                    </Col>
-                </FormGroup>
-                <FormGroup row>
-                    <Input required type="email" id="email" placeholder="email" />
-                </FormGroup>
-                <hr style={{ background: "rgba(255, 255, 255, 0.25)" }} />
-                <FormGroup row>
-                    <Input required type="password" id="password" placeholder="password" />
-                </FormGroup>
-                <FormGroup row>
-                    <InputGroup>
-                        <Input required type="password" id="conpassword" placeholder="confirm password" />
-                        <InputGroupAddon addonType="append">
-                            <Button color="success" style={{ borderRadius: 0 }}><Icon name="chevron-right" /></Button>
-                        </InputGroupAddon>
-                    </InputGroup>
-                </FormGroup>
-            </div>
-        );
-        if (this.state.loading) {
-            innerForm = (<div style={{ display: "block", width: "100%" }} align="center"><RingLoader color={theme.primary[0]} /> </div>);
-            innerText = "";
-        }
-        if (this.state.done) {
-            innerForm = (<Redirect to="/dashboard" />)
-            innerText = "";
-        }
-        let errors = null;
-        if (this.state.errors !== "") {
-            errors = (<Alert style={{ background: "rgba(255, 0, 0, 0.25)", border: "none", color: "white" }} color="danger">{this.state.errors}</Alert>)
-        }
-        let contents = (
-            <div style={{ padding: 30 }}>
-                <h1 className="display-1 theme-font">Sign Up</h1>
-                <p className="lead">{innerText}</p>
-                <Form onSubmit={(e) => {
-                    e.preventDefault();
-                    if (!this.state.loading) {
-                        this.setState({
-                            loading: true,
-                            errors: ""
-                        });
-                        let firstName = document.getElementById("first").value;
-                        let lastName = document.getElementById("last").value;
-                        let email = document.getElementById("email").value;
-                        let password = document.getElementById("password").value;
-                        let confirmPassword = document.getElementById("conpassword").value;
-                        this.props.profile.SignUp(firstName, lastName, email, password, confirmPassword, (msg) => {
-                            if (msg) {
-                                this.setState({
-                                    loading: false,
-                                    errors: msg
-                                });
-                            } else {
-                                this.setState({
-                                    loading: false,
-                                    done: true,
-                                    errors: ""
-                                });
-                            }
-                        });
-                    }
-                }}>
-                    {errors}
-                    {innerForm}
-                    <FormText><Link to="/login" style={{ color: "rgba(255, 255, 255, 0.5)" }}>Already a member? Login!</Link></FormText>
-                    <FormText><Link to="/" style={{ color: "rgba(255, 255, 255, 0.5)" }}>Return Home</Link></FormText>
-                </Form>
-            </div>
-        );
-        if (!this.props.isMobile) {
-            return (
-                <Container fluid style={{ width: "100%", minHeight: "100vh", textAlign: "center", backgroundColor: theme.secondary[1] }} className="d-flex align-items-center">
-                    <Col />
-                    <Col xs={3} style={{ display: "block", zIndex: 3, color: "white", background: "rgba(255, 255, 255, 0.05)" }}>
-                        {contents}
-                    </Col>
-                    <Col />
-                </Container>
-            );
-        } else {
-            return (
-                <Container fluid style={{ width: "100%", minHeight: "100vh", textAlign: "center", backgroundColor: theme.secondary[1] }} className="d-flex align-items-center">
-                    <Col xs={12} style={{ display: "block", zIndex: 3, color: "white", background: "rgba(255, 255, 255, 0.05)" }}>
-                        {contents}
-                    </Col>
-                </Container>
-            );
+        return <AuthForm
+            errors={this.state.errors}
+            label="Join us at HackRU!"
+            loading={this.state.loading}
+            isMobile={this.props.isMobile}
+            onSubmit={this.onSubmit}
+            title="Sign Up"
+        >
+            <FormGroup row>
+                <Col xs={6} style={{ margin: 0, paddingLeft: 0, paddingRight: 7 }}>
+                    <Input required id="first" type="text" placeholder="first name" />
+                </Col>
+                <Col xs={6} style={{ margin: 0, paddingRight: 0, paddingLeft: 7 }}>
+                    <Input required id="last" type="text" placeholder="last name" />
+                </Col>
+            </FormGroup>
+            <FormGroup row>
+                <Input required type="email" id="email" placeholder="email" />
+            </FormGroup>
+            <hr style={{ background: "rgba(255, 255, 255, 0.25)" }} />
+            <FormGroup row>
+                <Input required type="password" id="password" placeholder="password" />
+            </FormGroup>
+            <FormGroup row>
+                <InputGroup>
+                    <Input required type="password" id="conpassword" placeholder="confirm password" />
+                    <InputGroupAddon addonType="append">
+                        <Button color="success" style={{ borderRadius: 0 }}>
+                            <Icon name="chevron-right" />
+                        </Button>
+                    </InputGroupAddon>
+                </InputGroup>
+            </FormGroup>
+            <FormText>
+                <Link to="/login" style={{ color: "rgba(255, 255, 255, 0.5)" }}>
+                    Already a member? Login!
+                </Link>
+            </FormText>
+            <FormText>
+                <Link to="/" style={{ color: "rgba(255, 255, 255, 0.5)" }}>
+                    Return Home
+                </Link>
+            </FormText>
+        </AuthForm>;
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        if (!this.state.loading) {
+            this.setState({
+                loading: true,
+                errors: ""
+            });
+            let firstName = document.getElementById("first").value;
+            let lastName = document.getElementById("last").value;
+            let email = document.getElementById("email").value;
+            let password = document.getElementById("password").value;
+            let confirmPassword = document.getElementById("conpassword").value;
+            this.props.profile.SignUp(firstName, lastName, email, password, confirmPassword, (msg) => {
+                if (msg) {
+                    this.setState({
+                        loading: false,
+                        errors: msg
+                    });
+                } else {
+                    this.setState({
+                        loading: false,
+                        done: true,
+                        errors: ""
+                    });
+                }
+            });
         }
     }
 }
---
+++ src/library/AuthForm.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { Alert, Container, Col, Form } from "reactstrap";
+import { RingLoader } from "react-spinners";
+import { theme } from "../Defaults.js";
+import PropTypes from "prop-types";
+
+/*
+The basic form that authorization pages (sign up, log in, forgot password) use
+
+Props:
+- children: the body of the form itself
+- errors: a list of possible errors when communicating with the backend
+- label: indicate what the user should do
+- loading: if the form is in a loading state, and should show a spinner
+- isMobile: adjust the column widths for a mobile display
+- onSubmit: the actual function of the form
+- title: the form title
+*/
+const AuthForm = ({ children, errors, label, loading, isMobile, onSubmit, title }) => (
+    <Container
+        fluid
+        style={{ width: "100%", minHeight: "100vh", textAlign: "center", backgroundColor: theme.secondary[0] }}
+        className="d-flex align-items-center"
+    >
+        <Col
+            xs={isMobile ? 3 : 12}
+            style={{ display: "block", zIndex: 3, color: "white", background: "rgba(255, 255, 255, 0.05)" }}
+        >
+            <div style={{ padding: 30 }}>
+                <h1 className="display-1 theme-font">{ title }</h1>
+                <p className="lead">{loading ? label: ''}</p>
+                <Form onSubmit={onSubmit}>
+                    {renderErrors(errors)}
+                    <div>
+                        {loading ? renderSpinner(): children}
+                    </div>
+                </Form>
+            </div>
+        </Col>
+    </Container>
+);
+
+AuthForm.propTypes = {
+    children: PropTypes.node,
+    errors: PropTypes.array,
+    label: PropTypes.string,
+    loading: PropTypes.bool,
+    isMobile: PropTypes.bool,
+    onSubmit: PropTypes.func,
+    title: PropTypes.string,
+};
+
+const renderSpinner = () => (
+    <div style={{ display: "block", width: "100%" }} align="center">
+        <RingLoader color={theme.primary[0]} />
+    </div>
+);
+
+const renderErrors = (errors) => (
+    errors !== ""
+    ? (<Alert
+        style={{ background: "rgba(255, 0, 0, 0.25)", border: "none", color: "white" }}
+        color="danger">
+            {errors}
+        </Alert>)
+    : null
+);
+
+export default AuthForm;
