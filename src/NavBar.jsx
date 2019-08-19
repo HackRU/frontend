e78d669ecb191b3885ede7af2d5d5d79322fc1a0
Shivan Modha <shivanmodha@gmail.com>
2019-08-18 21:06:25 -0400

    Added responsiveness to NavBar

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,157 +1,94 @@
 import React, { Component } from 'react';
-import {
-  Navbar,
-  NavbarBrand,
-  Nav,
-  NavLink,
-  NavItem,
-  Collapse,
-  NavbarToggler,
-  Col,
-  Row,
-  Button,
-  ButtonGroup,
-  ButtonToolbar,
-  Container } from 'reactstrap';
-
-import GlowButton from "./components/GlowButton"
-import { defaults, navlinks, theme } from "./Defaults";
-
+import { Navbar, NavbarBrand, Nav, NavLink, NavItem, Collapse, NavbarToggler, Button, Container } from 'reactstrap';
+import { navlinks, theme } from "./Defaults";
 const buttonStyle = {
-  border: "1px solid white"
+    border: "1px solid white"
 }
-
 class NavBar extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.toggle = this.toggle.bind(this);
-    this.toggleIfMobile = this.toggleIfMobile.bind(this);
-
-    this.state = {
-      isOpen: false
-    };
-
-  }
-
-  componentDidMount() {
-    console.log(this.state.isOpen + "  " + this.props.isMobile)
-  }
-
-  toggle() {
-    if (this.props.isMobile) {
-      this.setState({
-        isOpen: !this.state.isOpen
-      });
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            isOpen: false
+        };
     }
-  }
-
-  toggleIfMobile() {
-    if (this.props.isMobile) {
-      this.toggle();
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
     }
-  }
-  
-
-  getAuthButtons() {
-    if (!this.props.isMobile) {
-      return(
-        <div>
-          <Button style={buttonStyle} color="link" href="/login">Login</Button>{' '}
-          <Button style={buttonStyle} color="link" href="/signup">Sign Up</Button>
-        </div>
-      );
-    }
-  }
-
-  getAuthLinks() {
-    return(
-      [<NavLink href="/login" color={theme.accent[1]}>Login</NavLink>,
-      <NavLink href="/signup" color={theme.accent[1]}>Sign Up</NavLink>
-      ]
-    );
-  }
-
-  getDashboardButton() {
-    return (
-      <Button href="/dashboard" color="link" style={buttonStyle}>Dashboard</Button>
-    )
-  }
-
-  getNavLinks() {
-    
-    let keys = Object.keys(navlinks);
-    let navLinks = [];
-    for (let i = 0; i < keys.length; i++) {
-      navLinks.push(
-        <NavItem>
-          <NavLink href={"/" + navlinks[keys[i]].url} onClick={this.toggleIfMobile}>{keys[i].toString()}</NavLink>
-        </NavItem>
-      );
-    }
-
-    return(
-      navLinks
-    )
-  }
-
-  getMobileNavbar() {
-    return(
-      <Container>
-        <NavItem>
-        <NavbarBrand onClick={this.toggle} style={{color:theme.accent[0]}}>HackRU</NavbarBrand>
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav navbar>
-              {this.getNavLinks()}
-              {this.getAuthLinks()}
-            </Nav>
-          </Collapse>
-        </NavItem>
-      </Container>
-      // [
-      // <NavItem>
-      //   <NavbarToggler onClick={this.toggle}/>
-      //   <Collapse isOpen={this.state.isOpen} navbar>
-      //     <Nav navbar>
-      //       {this.getNavLinks()}
-      //       {this.getAuthLinks()}
-      //     </Nav>
-      //   </Collapse>
-      // </NavItem>
-      //   ,
-      // <NavbarBrand href="/#" className="text-center" style={{color:theme.accent[0]}}>HackRU</NavbarBrand>
-      // ]
-    );
-  }
-
-  getNavbar() {
-    return(
-      [
-      <NavbarBrand href="/#" style={{color:theme.accent[0]}}>HackRU</NavbarBrand>,
-      this.getNavLinks()
-      ]
-    )
-  }
-
-  render() {
-
-    let profile = this.props.profile;
-    let isMobile = this.props.isMobile;
-
-    return (
-      <div>
-        <Navbar style={{width: "100%", zIndex: "20", backgroundColor: theme.secondary[1]}} fixed="top" dark expand="md">
-          <Container>
-            <Nav>
-              {isMobile ? this.getMobileNavbar() : this.getNavbar() }
-            </Nav>
-            <div pullRight className="ml-auto">
-              {profile.isLoggedIn ? this.getDashboardButton() : this.getAuthButtons()}
+    getAuthButtons() {
+        return (
+            <div>
+                <Button style={buttonStyle} color="link" href="/login">Login</Button>{' '}
+                <Button style={buttonStyle} color="link" href="/signup">Sign Up</Button>
             </div>
-          </Container>
-        </Navbar>
-      </div>
-    );
-  }
+        );
+    }
+    getAuthLinks() {
+        return (
+            [<NavLink href="/login" color={theme.accent[1]}>Login</NavLink>,
+            <NavLink href="/signup" color={theme.accent[1]}>Sign Up</NavLink>
+            ]
+        );
+    }
+    getDashboardButton() {
+        return (
+            <Button href="/dashboard" color="link" style={buttonStyle}>Dashboard</Button>
+        )
+    }
+    getNavLinks() {
+        let keys = Object.keys(navlinks);
+        let navLinks = [];
+        for (let i = 0; i < keys.length - 1; i++) {
+            navLinks.push(
+                <NavItem>
+                    <NavLink href={"/" + navlinks[keys[i]].url} onClick={this.toggleIfMobile}>{keys[i].toString()}</NavLink>
+                </NavItem>
+            );
+        }
+        return (
+            navLinks
+        )
+    }
+    getMobileNavbar() {
+        return (
+            <div>
+                <NavItem>
+                    <NavbarBrand onClick={this.toggle} style={{ color: theme.accent[0] }}>HackRU</NavbarBrand>
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav navbar>
+                            {this.getNavLinks()}
+                            {this.getAuthLinks()}
+                        </Nav>
+                    </Collapse>
+                </NavItem>
+            </div>
+        );
+    }
+    render() {
+        let profile = this.props.profile;
+        return (
+            <Navbar style={{ width: "100%", zIndex: "20", backgroundColor: theme.secondary[1] }} fixed="top" dark expand="md">
+                <Container>
+                    <NavbarBrand>
+                        <NavbarToggler onClick={this.toggle} style={{ marginRight: 10 }} />
+                        <a style={{ color: theme.accent[0] }} href="/#">HackRU</a>
+                    </NavbarBrand>
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav navbar className="mr-auto">
+                            {this.getNavLinks()}
+                        </Nav>
+                        <Nav navbar className="ml-auto">
+                            { profile.isLoggedIn ?
+                                this.getDashboardButton() :
+                                this.getAuthButtons()}
+                        </Nav>
+                    </Collapse>
+                </Container>
+            </Navbar>
+        );
+    }
 }
 export default NavBar;
