4227ba1607adb8074f9cdd279995a02ae984aa6a
Ryan Goldstein <ryanisaacg@gmail.com>
2019-08-23 21:44:58 -0400

    Refactor resume upload component

--- src/components/Dashboard/Forms/UserProfileForm/ResumeUploader.jsx
+++ src/components/Dashboard/Forms/UserProfileForm/ResumeUploader.jsx
@@ -1,62 +1,41 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { CustomInput, FormGroup } from "reactstrap";
 import { ProfileType } from "../../../Profile";
+import { theme } from "../../../../Defaults";
 import PropTypes from "prop-types";
 
-/**
- * A file picker widget that lets a user upload a resume to the S3 bucket
- * If the user has a resume, they may upload one again, but the widget will indicate they don't need to
- * The S3 bucket is configured to allow public writes, so no credentials are needed to perform the PUT query
- */
-class ResumeUploader extends Component {
-    /**
-     * Set the initial state
-     */
-    state = {
-        labelText: "Loading...",
+const ResumeUploader = ({ edit, profile }) => {
+    const [labelText, setLabelText] = useState("Loading...");
+
+    profile.DoesResumeExist().then(success => {
+        setLabelText(success ?
+            "Resume found" :
+            (edit ?
+                "Choose a file to upload" :
+                "Nothing yet")
+        );
+    });
+
+    const onUpload = async (event) => {
+        setLabelText("Uploading resume...");
+        // Make the upload request
+        const res = await profile.UploadResume(event.target.files[0]);
+        setLabelText(res.ok ? "Resume uploaded successfully." : "Failed to upload resume");
+    };
+    const pStyle = {
+        color: theme.disabled[0] + "70", padding: 5, height: 35
     };
 
-    constructor(props) {
-        console.log(props);
-        super(props);
-        props.profile.DoesResumeExist().then(success => {
-            this.setState({
-                labelText: success ?
-                    "Resume found" :
-                    (this.props.edit ?
-                        "Choose a file to upload" :
-                        "Nothing yet")
-            });
-        });
-    }
-
-    /**
-     * OnChange event handler
-     * @param {Event} event OnChange event handler event object
-     */
-    uploadHandler = async (event) => {
-        this.setState({ labelText: "Uploading resume..." });
-        // Make the upload request
-        const res = await this.props.profile.UploadResume(event.target.files[0]);
-        this.setState({
-            labelText: res.ok ? "Resume uploaded successfully." : "Failed to upload resume"
-        });
-    }
-    /**
-     * React render method
-     */
-    render = () => (
-        <div>
-            <h4> Upload a Resume </h4>
-            <FormGroup>
-                {this.props.edit ? <CustomInput accept=".pdf"
-                    id="resume"
-                    onChange={this.uploadHandler}
-                    type="file"
-                    label={this.state.labelText} /> : <p>{this.state.labelText}</p>}
-            </FormGroup>
-        </div>
-    )
+    return <div>
+        <h4> Upload a Resume </h4>
+        <FormGroup>
+            {edit ? <CustomInput accept=".pdf"
+                id="resume"
+                onChange={onUpload}
+                type="file"
+                label={labelText} /> : <p style={pStyle}>{labelText}</p>}
+        </FormGroup>
+    </div>;
 }
 
 ResumeUploader.propTypes = {
