68aac65b612b321abdf0c7a7893eacbe1a131841
thekabistro <kabir@kabirkuriyan.com>
2019-08-26 23:22:11 +0000

    Add SVG line to prod deploy

--- prod-deploy.sh
+++ prod-deploy.sh
@@ -6,4 +6,5 @@ npm run-script build
 #empty bucket, then upload
 aws s3 rm s3://hackru-frontend-prod --recursive
 aws s3 cp --recursive build s3://hackru-frontend-prod --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+aws s3 cp --recursive --exclude '*' --include '*.svg' --content-type 'image/svg+xml' build s3://hackru-frontend-prod --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
 aws cloudfront create-invalidation --distribution-id "E1YIAV5VULIMY7"  --paths "/*"
