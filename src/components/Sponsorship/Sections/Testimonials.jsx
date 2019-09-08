3a748f6f358533b5c7db0f4ff5704fa6821ac14e
thekabistro <kabir@kabirkuriyan.com>
2019-09-08 08:47:59 +0000

    Update testimonitals text

--- src/components/Sponsorship/Sections/Testimonials.jsx
+++ src/components/Sponsorship/Sections/Testimonials.jsx
@@ -14,10 +14,10 @@ class Testimonials extends Component {
                     <div class="hpanel testimo-panel text-center">
                         <div> 
                             <img alt="logo" class="img-fluid img-circle m-b"src="./assets/testimonials/t1.png"></img>
-                            <h3>Suzanne Link</h3>
-                            <div class="font-bold m-b-xs">Human Resource Management, Senior</div><hr />
+                            <h3>Sunny Feng</h3>
+                            <div class="font-bold m-b-xs">Electrical and Computer Engineering, Computer Science, 2020</div><hr />
                             <p>
-                                "Hi everyone! Last year I attended HackRU, and it was an awesome experience. Even as someone with no background in ITI/ Computer Science, I was welcomed and included. I had a great time meeting new people and learning new skills. 10/10 would recommend!"
+                               Not only was I able to work on personal projects at HackRU, I also got to interact with its sponsoring companies. At my first HackRU, I won s challenge with a healthy shopping app and got to present my hack to the s Vice President of Technology. This led me to an internship with Wakefern the summer after my freshman year. At my second HackRU, I won s challenge with a music/movie data science project and was connected to the recruiter there. HackRU has allowed me to establish close relationships with companies in a way I couldn't have otherwise.
                             </p>
                         </div>
                     </div>
@@ -26,10 +26,10 @@ class Testimonials extends Component {
                     <div class="hpanel testimo-panel text-center">
                         <div>
                             <img alt="logo" class="img-fluid img-circle m-b" src="./assets/testimonials/t2.png"></img>
-                            <h3>Rishab Chawla</h3>
-                            <div class="font-bold m-b-xs">Computer Science, Junior</div><hr />
+                            <h3>Naeem Hossain</h3>
+                            <div class="font-bold m-b-xs">Computer Science, Information Technology and Informatics, 2018</div><hr />
                             <p>
-                                "Built from the ground up, HackRU was the first hackathon to express the true essence of flavor. Capturing the natural characteristics of only the finest ingredients, HackRU is exceptional in taste - thanks to the personal commitment of the sponsors and organizers."
+                                "Hackathons are a really organic way to make friends and find a niche in the tech community. At HackRU, I was able to build hacks with incredibly talented people, travel to lots of new places, and drink irresponsible amounts of coffee! I obtained a summer internship with Prudential for my junior year thanks to HackRU. I also loved attending for some superficial (but still valid) reasons - like playing four games of Jenga in a row to get pajamas!"
                             </p>
                         </div>
                     </div>
@@ -38,10 +38,10 @@ class Testimonials extends Component {
                     <div class="hpanel testimo-panel text-center">
                         <div >
                             <img alt="logo" class="img-fluid img-circle m-b" src="./assets/testimonials/t3.png"></img>
-                            <h3>Elaine Huang</h3>
-                            <div class="font-bold m-b-xs">Neuroscience and Philosophy, Senior</div><hr />
+                            <h3>Mihai Andrei</h3>
+                            <div class="font-bold m-b-xs"> Mathematics & Computer Science, 2019</div><hr />
                             <p>
-                                "HackRU is where my programming journey began-- I actually downloaded Python at my first HackRU. The focused and collaborative environment, coupled with the presence of patient mentors, was incredibly conducive to learning. I felt welcome even as a complete novice to computer science!"
+			    "I was given the opportunity to speak with company representatives and recruiters sponsoring the event and some of the conversations I had eventually turned into internship offers for the summer. In fact, my sophomore summer internship at Prudential and my junior summer internship at MongoDB arose from the connections I made and the network I built at Rutgers. In addition, my involvement with HackRU as the 2018-19 Executive Director allowed me to build a stronger Computer Science community and make many friends. I was also able to sharpen my leadership skills. Without a doubt, my involvement with HackRU has been one of the most important experiences during my time at Rutgers."				
                             </p>
                         </div>
                     </div>
@@ -50,10 +50,10 @@ class Testimonials extends Component {
                     <div class="hpanel testimo-panel text-center">
                         <div>
                             <img alt="logo" class="img-fluid img-circle m-b" src="./assets/testimonials/t4.png"></img>
-                            <h3>Ben Yang</h3>
-                            <div class="font-bold m-b-xs">Rutgers School of Arts and Science</div><hr />
+                            <h3>Joyce Van Drost</h3>
+                            <div class="font-bold m-b-xs"> Psychology & Computer Science, 2018</div><hr />
                             <p>
-                                "HackRU Spring 2018 was a really fun and exciting experience - from food to workshops to speakers, the venue really had everything! But aside from all that, it was also a really great experience to just sit down and learn with other people, regardless of whether they were my friends or not. While I wasn't able to finish my project, I was really happy that I learned something valuable at the end of the day."
+                                "My two years as Marketing Director for HackRU has really shaped my college experience. I was able to better my marketing skills with social media marketing while learning more about graphic design. I was also able to improve upon my public speaking skills by taking the lead on public announcements. Overall, it was great to get to know and interact with the community and build relationships throughout each new hackathon I attended. In fact, HackRU helped me land the job I have today! Interacting with all the Sponsors at the event was always a good time and it was great to learn about their respective companies in a smaller and more intimate exchange just by walking up to their table."
                             </p>
                         </div>
                     </div>
