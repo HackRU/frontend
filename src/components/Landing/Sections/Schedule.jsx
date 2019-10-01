b4a9dd800b399481e265e74deb43e08889ef08fc
ryanmao725 <ryan.mao.725@gmail.com>
2019-10-01 16:41:52 -0400

    Kabir still dictator but ran eslint

--- src/components/Landing/Sections/Schedule.jsx
+++ src/components/Landing/Sections/Schedule.jsx
@@ -1,14 +1,13 @@
 import React, { Component } from "react";
-import GlowButton from "../../GlowButton";
-import { defaults } from "../../../Defaults";
-import { Table, Card, Container, Col, Row } from 'reactstrap';
+// import { defaults } from "../../../Defaults";
+import { Table, Card, Container, Col, Row } from "reactstrap";
 
 /**
  * Schedule component for the landing page
  */
 
-const saturday_date = "10/19/2019"
-const sunday_date = "10/20/2019"
+const saturday_date = "10/19/2019";
+const sunday_date = "10/20/2019";
 
 const sat_schedule = [
     ["10:00 AM","Check-In","Main Lobby"],
@@ -16,7 +15,7 @@ const sat_schedule = [
     ["11:30 AM", "Opening Ceremonies", "Main Stage"],
     ["1:00 PM", "Hacking Begins", "Hacking Stations"],
     ["6:00 PM", "Dinner", "Food Table"],
-]
+];
 
 const sun_schedule = [
     ["12:00 AM", "Late Night Snack", "(it's a surprise)"],
@@ -26,26 +25,26 @@ const sun_schedule = [
     ["10:30 AM", "Lunch", "Food Table"],
     ["12:00 PM", "Demos Begin", "Hacking Area"],
     ["3:00 PM", "Closing Ceremonies", "Main Stage"]
-]
+];
 
 const light_red = {
     backgroundColor: "#bf4d4d",
     border: "none !important"
-}
+};
 
 const dark_red = {
     backgroundColor: "#ad4444",
     border: "none !important"
-}
+};
 
 class Schedule extends Component {
 
     constructor(props) {
-        super(props)
+        super(props);
 
         this.state = {
-            activeTab: 'sat'
-        }
+            activeTab: "sat"
+        };
     }
 
     fill_table = (schedule) => {
@@ -79,24 +78,29 @@ class Schedule extends Component {
     render() {
         return (
             [
-            <div className="col-12">
-                <h1 className="display-4 theme-font">Schedule</h1>
-                <hr/>
-            </div>,
-            <Card className="shadow-lg rounded-0" style={{backgroundColor:"#ad4444", margin: "0", padding: "0"}}>
-                <Container fluid>
-                    <Row>
-                        <Col xs="12" md="6">
-                            <h3 className="mt-2 text-center">SATURDAY {saturday_date}</h3>
-                            {this.fill_table(sat_schedule)}
-                        </Col>
-                        <Col xs="12" md="6">
-                            <h3 className="mt-2 text-center">SUNDAY {sunday_date}</h3>
-                            {this.fill_table(sun_schedule)}
-                        </Col>
-                    </Row>
-                </Container>
-            </Card>]
+                <div className="col-12"
+                    key="schedule-header">
+                    <h1 className="display-4 theme-font">Schedule</h1>
+                    <hr/>
+                </div>,
+                <Card className="shadow-lg rounded-0"
+                    key="schedule-card"
+                    style={{backgroundColor:"#ad4444", margin: "0", padding: "0"}}>
+                    <Container fluid>
+                        <Row>
+                            <Col xs="12"
+                                md="6">
+                                <h3 className="mt-2 text-center">SATURDAY {saturday_date}</h3>
+                                {this.fill_table(sat_schedule)}
+                            </Col>
+                            <Col xs="12"
+                                md="6">
+                                <h3 className="mt-2 text-center">SUNDAY {sunday_date}</h3>
+                                {this.fill_table(sun_schedule)}
+                            </Col>
+                        </Row>
+                    </Container>
+                </Card>]
         );
     }
 }
