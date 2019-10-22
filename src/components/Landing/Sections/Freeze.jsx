822f7f30bb1a6177c3b2b9492dcde2bfb2f1693c
Shivan Modha <shivanmodha@gmail.com>
2019-10-22 17:45:35 -0400

    Links bound to middle portion

--- src/components/Landing/Sections/Freeze.jsx
+++ src/components/Landing/Sections/Freeze.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Container, Row, Button } from "reactstrap";
+import { Container, Row, Col, Button, ButtonGroup } from "reactstrap";
 import { TextGrid } from "../../AnimatedGrid";
 
 class Freeze extends Component {
@@ -25,32 +25,31 @@ class Freeze extends Component {
         this._event_onResize();
     }
     render() {
+        let leftPos = (window.innerWidth / 2) - this.state.subWidth + 10;
         return (
             <Container fluid
                 id="landing-section"
-                style={{ zIndex: 3,  width: "100%", minHeight: "100vh", textAlign: "center", userSelect: "none" }}>
-                <Row style={{ minWidth: "100%", minHeight: "100vh" }} >
-                    <div style={{ display: "block", position: "absolute", top: window.innerHeight / 2, left: (window.innerWidth / 2) - this.state.subWidth + 10 }}>
+                style={{ zIndex: 3,  width: "100%", minHeight: "100vh", userSelect: "none" }}>
+                <Row style={{ minWidth: "100%", minHeight: "100vh", textAlign: "right" }} >
+                    <div style={{ display: "block", position: "absolute", width: window.innerWidth - (2 * leftPos), top: window.innerHeight / 2, left: leftPos}}>
                         <TextGrid ref="anime"
                             text={"HACKRU WILL RETURN"} />
-                        <div style={{ marginTop: 75 }}>
-
-                        </div>
+                        <br/>
+                        <br/>
+                        <i><h4>for Spring 2020</h4></i>
+                        <br/>
+                        <Row style={{ marginLeft: 8, width: "100%", backgroundColor: "rgba(0, 0, 0, 0.01)", padding: 10, textAlign: "center" }}>
+                            <Col xs={6}>
+                                <i><p style={{ marginBottom: 3, color: "rgba(255, 255, 255, 0.5)" }}>Stay in the know!</p></i>
+                                <a href="https://hackru1.typeform.com/to/OEtQEO"><Button color="light" style={{ border: "none" }} outline>Join Mailing List</Button></a>
+                            </Col>
+                            <Col xs={6}>
+                                <i><p style={{ marginBottom: 3, color: "rgba(255, 255, 255, 0.5)" }}>Past Submissions</p></i>
+                                <a href="https://hackru-s19.devpost.com"><Button color="light" style={{ border: "none" }} outline>Spring 2019</Button></a>
+                                <a href="https://hackru-f19.devpost.com"><Button color="light" style={{ border: "none" }} outline>Fall 2019</Button></a>
+                            </Col>
+                        </Row>
                     </div>
-                </Row>
-                <Row style={{ marginTop: "-55%" }}
-                    className="d-flex justify-content-center" >
-                    <h4>for Spring 2020</h4>
-                </Row>
-                <Row style={{marginTop: 20}}
-                    className="d-flex justify-content-center" >
-                    <a href="https://hackru1.typeform.com/to/OEtQEO"><Button outline
-                        className="">Join the mailing list</Button></a>
-                </Row>
-                <Row style={{marginTop: 10}}
-                    className="d-flex justify-content-center" >
-                    <a href="https://hackru-f19.devpost.com"><Button outline
-                        className="">Check out past submissions</Button>  </a>  
                 </Row>
             </Container>
         );
