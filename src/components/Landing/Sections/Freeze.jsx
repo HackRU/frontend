ebed7624dc0cbc15d2e68fa618158c48fd2097a2
Shivan Modha <shivanmodha@gmail.com>
2019-10-22 16:50:50 -0400

    From fone to desktop

--- src/components/Landing/Sections/Freeze.jsx
+++ src/components/Landing/Sections/Freeze.jsx
@@ -28,34 +28,30 @@ class Freeze extends Component {
         return (
             <Container fluid
                 id="landing-section"
-                style={{ zIndex: 3, width: "100%", minHeight: "100vh", textAlign: "center", userSelect: "none" }}>
-
+                style={{ zIndex: 3,  width: "100%", minHeight: "100vh", textAlign: "center", userSelect: "none" }}>
                 <Row style={{ minWidth: "100%", minHeight: "100vh" }} >
-                    <div style={{ display: "block", position: "absolute", top: window.innerHeight / 2, left: (window.innerWidth / 2) - this.state.subWidth }}>
+                    <div style={{ display: "block", position: "absolute", top: window.innerHeight / 2, left: (window.innerWidth / 2) - this.state.subWidth + 10 }}>
                         <TextGrid ref="anime"
                             text={"HACKRU WILL RETURN"} />
                         <div style={{ marginTop: 75 }}>
 
                         </div>
                     </div>
-
                 </Row>
-                <div style={{ display: "block", position: "absolute", bottom: window.innerHeight / 8, width: "100%", left: "0" }}>
-                    <Row style ={{}}
-                        className="d-flex justify-content-center" >
-                        <h4>for Fall 2019</h4>
-                    </Row>
-                    <Row style={{marginTop: 20}}
-                        className="d-flex justify-content-center" >
-                        <a href="https://hackru1.typeform.com/to/OEtQEO"><Button outline
-                            className="">Join the mailing list</Button></a>
-                    </Row>
-                    <Row style={{marginTop: 10}}
-                        className="d-flex justify-content-center" >
-                        <a href="https://hackru-s19.devpost.com"><Button outline
-                            className="">Check out past submissions</Button>  </a>  
-                    </Row>
-                </div>
+                <Row style={{ marginTop: "-55%" }}
+                    className="d-flex justify-content-center" >
+                    <h4>for Spring 2020</h4>
+                </Row>
+                <Row style={{marginTop: 20}}
+                    className="d-flex justify-content-center" >
+                    <a href="https://hackru1.typeform.com/to/OEtQEO"><Button outline
+                        className="">Join the mailing list</Button></a>
+                </Row>
+                <Row style={{marginTop: 10}}
+                    className="d-flex justify-content-center" >
+                    <a href="https://hackru-f19.devpost.com"><Button outline
+                        className="">Check out past submissions</Button>  </a>  
+                </Row>
             </Container>
         );
     }
