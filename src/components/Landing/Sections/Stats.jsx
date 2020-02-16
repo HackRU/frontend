0c566fb51963ff6cca5438916e15496bac28ddb5
Shivan Modha <shivanmodha@gmail.com>
2020-02-16 10:12:45 -0500

    Fixed Mobile Number Bug

--- src/components/Landing/Sections/Stats.jsx
+++ src/components/Landing/Sections/Stats.jsx
@@ -23,9 +23,11 @@ const Stats = () => (
 const Stat = ({ number, text, back, accent, icon }) => (
     <div style={{ backgroundColor: back, color: "white", boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.5)", padding: 50, marginTop: 25 }}>
         <div style={{ position: "absolute", left: "calc(15px)", top: 25, height: "calc(100% - 25px)", backgroundColor: accent, width: 10 }}></div>
-        <h1 className="display-2">{number} </h1>
-        <h1>  {text}      <Icon name={icon} style={{ marginRight: 10, color: accent }} />
- </h1>
+        <div>
+            <h1 className="stat-number">{number} </h1>
+            <h1>{text}</h1>
+        </div>
+        <Icon name={icon} style={{ fontSize: "10vh", marginRight: 10, color: accent }} />
     </div>
 );
 
