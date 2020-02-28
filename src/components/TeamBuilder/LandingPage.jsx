af46dee234145e6def77060621e9d25a21f30d68
benlambert43 <benlambert43@gmail.com>
2020-02-28 17:04:19 -0500

    floating page design changes

--- src/components/TeamBuilder/LandingPage.jsx
+++ src/components/TeamBuilder/LandingPage.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import './styling.css'
-import { Container } from "reactstrap";
 
 const LandingPage = (props) => {
   console.log(props)
   return (
-    <Container className="page-view">
+    <div className="page-view">
       <div>
       <h1> LandingPage Component</h1>
       </div>
-    </Container>
+    </div>
   );
 };
 
