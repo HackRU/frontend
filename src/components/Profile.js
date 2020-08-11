1f802222a0f77f265eeed59caf2d1d3b4ca9281a
Gabriel Weinbrenner <angelgabe101@gmail.com>
2020-08-11 14:34:57 -0400

    Travis Fixes

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -107,14 +107,14 @@ class Profile {
     }
     /* Parses the JWT token to get the data */
     parseJwt(token) {
-        var base64Url = token.split('.')[1];
-        var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-        var jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
-            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-        }).join(''));
+        var base64Url = token.split(".")[1];
+        var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+        var jsonPayload = decodeURIComponent(atob(base64).split("").map(function (c) {
+            return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(""));
 
         return JSON.parse(jsonPayload);
-    };
+    }
 
     Login(email, password, callback) {
         if (this.isLoggedIn) {
