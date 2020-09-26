218d69e584a28398fbf1fdac25d808f04cedd6b3
Zain Ali <zain08816@gmail.com>
2020-09-25 23:42:22 -0600

    add slack_id regex validation

--- src/components/Dashboard/Forms/UserProfileForm/ProfileCards/Communications.jsx
+++ src/components/Dashboard/Forms/UserProfileForm/ProfileCards/Communications.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
-import { FormGroup, Input, Label, Button, UncontrolledAlert } from "reactstrap";
-import { AvForm } from "availity-reactstrap-validation";
+import { FormGroup, Label, Button, UncontrolledAlert } from "reactstrap";
+import { AvForm, AvField } from "availity-reactstrap-validation";
 import { theme } from "../../../../../Defaults";
 import { Icon } from "react-fa";
 import { ProfileType } from "../../../../Profile";
 import PropTypes from "prop-types";
+import { PulseLoader } from "react-spinners";
 
 class UserProfileForm extends Component {
     constructor(props) {
@@ -14,86 +15,127 @@ class UserProfileForm extends Component {
     UNSAFE_componentWillMount() {
         this.setState({
             user: this.props.user,
-            edit: (this.props.user.registration_status === "unregistered"),
-            message: null
+            edit: this.props.user.registration_status === "unregistered",
+            error: false,
+            message: null,
+            loading: false,
         });
     }
 
     updateUser(user) {
         console.log(user);
         this.setState({
-            user
+            user,
         });
-        this.props.onChange(user);
+        // this.props.onChange(user);
+    }
+
+    submitUser = (user) => {
+        this.setState({
+            profileMSG: null,
+            loading: true,
+            user,
+        }, () => {
+            this.props.profile.Set(this.state.user, (err) => {
+                console.log(err);
+                if( err) {
+                    this.setState({
+                        message: "Invalid Slack ID",
+                        error: true,
+                        loading: false,
+                        profileMSG: { color: "danger", value: err } 
+                    });
+                } else {
+                    this.setState({
+                        edit: false,
+                        loading: false,
+                        profileMSG: err ?
+                            { color: "danger", value: err } :
+                            { color: "success", value: "Profile Updated!" }
+                    });
+                }
+            });
+        });
     }
 
     render() {
         // let mobile = this.props.mobile;
         let user = this.state.user;
-        let message = null;
-        if (this.state.message) {
-            message = (<UncontrolledAlert color="danger">{this.state.message}</UncontrolledAlert>);
-        }
+        // let message = null;
+        // if (this.state.message) {
+        //     message = <UncontrolledAlert color="danger">{this.state.message}</UncontrolledAlert>;
+        // }
         if (this.state.edit) {
             return (
-                <AvForm 
+                <AvForm
                     onValidSubmit={() => {
-                        this.props.onSubmit(this.state.user);
+                        this.submitUser(this.state.user);
                     }}
                     onInvalidSubmit={() => {
-                        this.setState({ message: null }, () => { this.setState({ message: "Some fields are invalid." }); });
-                    }}>
-
+                        this.setState({ message: null }, () => {
+                            this.setState({ message: "Some fields are invalid." });
+                        });
+                    }}
+                >
                     <FormGroup>
-                        <Label for="github">GitHub Handle</Label>
-                        <Input id="github"
+                        <AvField
+                            name="slack"
+                            label="Slack ID"
+                            type="text"
+                            placeholder="hackru"
+                            value={user.slack_id}
+                            onChange={(e) => { user.slack_id = e.target.value; this.updateUser(user); }}
+                            validate={{
+                                required: {value: false },
+                                pattern: { value: "^[UW][A-Z0-9]{2,}$", errorMessage: "Invalid Slack ID" } }} />
+                    </FormGroup>
+                    <FormGroup>
+                        <AvField
+                            name="github"
+                            label="Github Handle"
                             type="text"
                             placeholder="hackru"
                             value={user.github}
                             onChange={(e) => { user.github = e.target.value; this.updateUser(user); }} />
                     </FormGroup>
-                    <FormGroup>
-                        <Label for="github">GitHub Handle</Label>
-                        <Input id="github"
-                            type="text"
-                            placeholder="hackru"
-                            value={user.github}
-                            onChange={(e) => { user.github = e.target.value; this.updateUser(user); }} />
-                    </FormGroup>
-                    {message}
-                    <div style={{ width: "100%" }}
+                    {this.state.error ? <UncontrolledAlert color="danger">{this.state.message}</UncontrolledAlert> : ""}
+                    <div style={{ width: "100%" }} 
                         align="right">
-                        <Button className="pill-btn"
-                            color="warning"
-                            style={{ marginRight: 10 }}
-                            type="reset">Clear</Button>
-                        <Button color="success"
-                            className="pill-btn"
-                            type="submit">Update</Button>
+                        <Button color="success" 
+                            className="pill-btn" 
+                            type="submit"> { this.state.loading ?  <PulseLoader color={theme.accent[0]} /> : "Update" } </Button>
                     </div>
                 </AvForm>
             );
         } else {
             let pStyle = {
-                color: theme.disabled[0], padding: 5, minHeight: 35
+                color: theme.disabled[0],
+                padding: 5,
+                minHeight: 35,
             };
             let field = (text) => {
-                return <p style={pStyle}>{(text) ? text : <i>unanswered</i> }</p>;
+                return <p style={pStyle}>{text ? text : <i>unanswered</i>}</p>;
             };
             return (
                 <div>
                     <h4>
-                        <Button color="primary"
+                        <Button
+                            color="primary"
                             className="pill-btn"
                             style={{ position: "absolute", right: 40 }}
-                            onClick={() => { this.setState({ edit: true }); }} ><Icon name="edit" /></Button>
+                            onClick={() => {
+                                this.setState({ edit: true });
+                            }}
+                        >
+                            <Icon name="edit" />
+                        </Button>
                     </h4>
                     <FormGroup>
-                        <Label>GitHub Handle</Label>
-                        {field(user.github)}
+                        <Label>Slack ID</Label>
+                        {field(user.slack_id)}
                     </FormGroup>
                     <FormGroup>
-                        <Label>Slack Handle</Label>
+                        <Label>GitHub Handle</Label>
                         {field(user.github)}
                     </FormGroup>
                 </div>
@@ -101,7 +143,6 @@ class UserProfileForm extends Component {
         }
     }
 }
-
 
 UserProfileForm.propTypes = {
     user: {
