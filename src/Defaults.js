11a4d5ad3bccd6691d12e7bc86502616ecfe540d
Shivan Modha <shivanmodha@gmail.com>
2020-09-27 13:36:26 -0400

    Updated waiver

--- src/Defaults.js
+++ src/Defaults.js
@@ -31,7 +31,7 @@ const defaults =  {
     "volunteers": {
         "display": true,
         "vol_url": "https://docs.google.com/forms/d/e/1FAIpQLSe_qBzqsJIKWd_BohZ7Xuju3XQhI2f6xtrwX7WO-otR0Q7ofg/viewform?usp=sf_link",
-        "mentor_url": "https://docs.google.com/forms/d/1hR-R6bpZoLAlPZFyoNYUquc7x8JX4ydWvZoCXckCmCU"
+        "mentor_url": "https://docs.google.com/forms/d/e/1FAIpQLScG3eyzY67q58k52zh5nSscUBWRoYn8KayVNAXqYvJl8oqD8Q/viewform"
     },
     "dayof": false
 };
