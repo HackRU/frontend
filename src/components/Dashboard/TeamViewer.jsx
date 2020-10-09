401d15ded3a7bf70f4f09924cfdeeca683780e37
Gabriel Weinbrenner <angelgabe101@gmail.com>
2020-10-09 01:42:03 -0400

    Design of team viewer

--- src/components/Dashboard/TeamViewer.jsx
+++ src/components/Dashboard/TeamViewer.jsx
@@ -1,8 +1,22 @@
 import React, { useState, useEffect } from "react";
-import { Container, Grid } from "@material-ui/core";
+import { Container, Grid, Avatar, Typography, AppBar, Tabs, Tab, Box } from "@material-ui/core";
 import Section from "./Section";
 
+function a11yProps(index) {
+    return {
+      id: `simple-tab-${index}`,
+      'aria-controls': `simple-tabpanel-${index}`,
+    };
+  }
+
+
 const TeamViewer = (props) => {
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+      setValue(newValue);
+    };
+  
     return(
         <Container maxWidth={false} 
             style={{paddingTop: 90 }}>
@@ -12,13 +26,41 @@ const TeamViewer = (props) => {
                         subtitle="Introduce yourself, don't be shy!"
                         color="yellow"
                         isOpen={true}>
+                            <Grid container direction="column" alignItems="center">
+                                <Grid container direction="row" justify="center">
+                                    <Grid item style={{"margin-right": "2em"}}>
+                                        <Avatar style={{"width": "6em", "height": "6em"}}>E</Avatar>
+                                    </Grid>
+                                    <Grid item direction="column">
+                                        <Grid item>
+                                            <Typography variant="h5">Example Example</Typography>
+                                        </Grid>
+                                        <Grid item>
+                                            <Typography variant="h7">Bio</Typography>
+                                        </Grid>
+                                        <Grid item>
+                                            <Typography variant="h7">Skills:</Typography>
+                                        </Grid>
+                                        <Grid item>
+                                            <Typography variant="h7">Interests:</Typography>
+                                        </Grid>
+                                        <Grid item>
+                                            <Typography variant="h7">Prizes:</Typography>
+                                        </Grid>
+                                    </Grid>
+                                </Grid>
+                                <Grid item style={{marginTop: "2em"}}>
+                                    <AppBar position="static" color="transparent" style={{ background: 'transparent', boxShadow: 'none'}}>
+                                        <Tabs value={value} variant="fullWidth" onChange={handleChange} aria-label="simple tabs example">
+                                            <Tab label="My Team" {...a11yProps(0)} />
+                                            <Tab label="Edit Team" {...a11yProps(1)} />
+                                            <Tab label="Find Teams" {...a11yProps(2)} />
+                                        </Tabs>
+                                    </AppBar>
+
+                                </Grid>
+                            </Grid>
                     </Section>
-                </Grid>
-                <Grid item
-                    xs={12}
-                    sm={12}
-                    md={6}>
-                        
                 </Grid>
             </Grid>
         </Container>
