15bad32b6c77f14ec14b75985c38899a493ea23c
shivanmodha <shivanmodha@gmail.com>
2020-10-15 22:44:35 -0400

    removed early return

--- src/MLHBadge.jsx
+++ src/MLHBadge.jsx
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 
 class MLHBadge extends Component {
     render() {
-        return (<></>);
         return (
             <a id="mlh-trust-badge"
                 style={{ display: "block", maxWidth: "100px", minWidth: "60px", position: "absolute", right: "50px", top: "0", width: "10%", zIndex: "10" }}
