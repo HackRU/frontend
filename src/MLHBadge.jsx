a80d5248608ab6bb52b512d995736a4f200043a0
Zain Ali <zain08816@gmail.com>
2020-11-03 12:16:25 -0700

    Change badge to 2021

--- src/MLHBadge.jsx
+++ src/MLHBadge.jsx
@@ -5,11 +5,11 @@ class MLHBadge extends Component {
         return (
             <a id="mlh-trust-badge"
                 style={{ display: "block", maxWidth: "100px", minWidth: "60px", position: "absolute", right: "50px", top: "0", width: "10%", zIndex: "10" }}
-                href="https://mlh.io/seasons/na-2020/events?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2020-season&utm_content=white"
+                href="https://mlh.io/seasons/2021/events?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2021-season&utm_content=white"
                 target="_blank"
                 rel="noopener noreferrer">
-                <img src="https://s3.amazonaws.com/logged-assets/trust-badge/2020/mlh-trust-badge-2020-black.svg"
-                    alt="Major League Hacking 2020 Hackathon Season"
+                <img src="https://s3.amazonaws.com/logged-assets/trust-badge/2021/mlh-trust-badge-2021-black.svg"
+                    alt="Major League Hacking 2021 Hackathon Season"
                     style={{ width: "100%" }} />
             </a>
         );
