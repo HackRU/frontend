605c7b4c7fec82a22c222f0162eb22ef744adbf3
Zain Ali <zain08816@gmail.com>
2020-11-06 16:23:06 -0700

    Change Forget Prompt

--- src/components/Forgot.jsx
+++ src/components/Forgot.jsx
@@ -1,38 +1,39 @@
 import React, { useState } from "react";
-import { Container, Grid, createMuiTheme, ThemeProvider} from "@material-ui/core";
+// import { Container, Grid, createMuiTheme, ThemeProvider} from "@material-ui/core";
+import { Container, Grid } from "@material-ui/core";
 import Alert from "@material-ui/lab/Alert";
 import { Link } from "react-router-dom";
 import { Redirect } from "react-router-dom";
 import AuthForm from "../library/AuthForm";
 import ColorButton from "../library/ColorButton";
-import WhiteTextField from "../library/WhiteTextField";
+// import WhiteTextField from "../library/WhiteTextField";
 import PropTypes from "prop-types";
-import { theme } from "../Defaults";
+// import { theme } from "../Defaults";
 
 /**
  * Forgot my password application for "/forgot"
  */
-const color_theme = createMuiTheme({
+// const color_theme = createMuiTheme({
     
-    overrides: {
-        palette: {
-            primary: {
-                main: theme.primary[1].trim(),
-            },
-            secondary: {
-                main: theme.secondary[1].trim(),
-            },
-        },
-        MuiInputLabel: { 
-            root: { 
-                color: "white",
-                "&$focused": { 
-                    color: "white"
-                }
-            }
-        }
-    }
-});
+//     overrides: {
+//         palette: {
+//             primary: {
+//                 main: theme.primary[1].trim(),
+//             },
+//             secondary: {
+//                 main: theme.secondary[1].trim(),
+//             },
+//         },
+//         MuiInputLabel: { 
+//             root: { 
+//                 color: "white",
+//                 "&$focused": { 
+//                     color: "white"
+//                 }
+//             }
+//         }
+//     }
+// });
 
 
 const ForgotPage = (props) => {
@@ -97,7 +98,7 @@ const ForgotPage = (props) => {
                     spacing={2}>
                     <Grid item 
                         xs={12}>
-                        <ThemeProvider theme={color_theme}>
+                        {/* <ThemeProvider theme={color_theme}>
                             <WhiteTextField
                                 variant="outlined"
                                 autofocus
@@ -109,11 +110,14 @@ const ForgotPage = (props) => {
                                 autoComplete="email"
                                 size="small"
                             />
-                        </ThemeProvider>
+                        </ThemeProvider> */}
+                        <div>
+                        Please reach out to rnd@hackru.org to reset password
+                        </div>
                     </Grid>
                     <Grid item 
                         xs={12}>
-                        <ThemeProvider theme={color_theme}>
+                        {/* <ThemeProvider theme={color_theme}>
                             <ColorButton
                                 size = "small"
                                 type="submit"
@@ -123,7 +127,19 @@ const ForgotPage = (props) => {
                                 Submit
                             </ColorButton>
 
-                        </ThemeProvider>
+                        </ThemeProvider> */}
+                        <a href="mailto:rnd@hackru.org?subject=[HackRU Password Reset]"
+                            target="_blank" 
+                            rel="noopener noreferrer">
+                            <ColorButton
+                                size = "small"
+                                type="button"
+                                fullWidth
+                                variant="contained"
+                                color="primary">
+                                Email Us
+                            </ColorButton>
+                        </a>
                     </Grid>
                     <Grid item 
                         xs={12}>
