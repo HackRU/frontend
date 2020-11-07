71d37f3ca9f50897095db398a8f4423cdb064176
shivanmodha <shivanmodha@gmail.com>
2020-11-06 22:41:11 -0500

    Lint-fixes

--- src/components/Live/Schedule.jsx
+++ src/components/Live/Schedule.jsx
@@ -3,26 +3,34 @@ import request from "request";
 import { ENDPOINTS } from "../Profile";
 import { ListGroup } from "reactstrap";
 import { Grid } from "@material-ui/core";
-import { SyncLoader } from "react-spinners";
 import { ViewState } from "@devexpress/dx-react-scheduler";
 import { Scheduler, DayView, Appointments, AppointmentTooltip } from "@devexpress/dx-react-scheduler-material-ui";
 import { theme } from "../../Defaults";
 import RoomIcon from "@material-ui/icons/Room";
-import DescriptionIcon from "@material-ui/icons/Description";
 import PropTypes from "prop-types";
 
-const Content = ({ children, appointmentData, classes, ...restProps }) => (
-    <AppointmentTooltip.Content {...restProps} appointmentData={appointmentData}>
-        <Grid container alignItems="center">
-            <Grid item xs={10} style={{ paddingLeft: 20, paddingBottom: 10, color: "rgba(150, 150, 150, 1)" }}>
-                <RoomIcon /> <span style={{ paddingLeft: 17 }} dangerouslySetInnerHTML={{ __html: appointmentData.location ? appointmentData.location : "<i>nothing to see here</i>" }} />
+const Content = ({ appointmentData, ...restProps }) => (
+    <AppointmentTooltip.Content {...restProps}
+        appointmentData={appointmentData}>
+        <Grid container
+            alignItems="center">
+            <Grid item
+                xs={10}
+                style={{ paddingLeft: 20, paddingBottom: 10, color: "rgba(150, 150, 150, 1)" }}>
+                <RoomIcon /> <span style={{ paddingLeft: 17 }}
+                    dangerouslySetInnerHTML={{ __html: appointmentData.location ? appointmentData.location : "<i>nothing to see here</i>" }} />
             </Grid>
-            <Grid item xs={10} style={{ marginLeft: 30, paddingTop: 10,  borderTop: "1px solid rgba(200, 200, 200, 1)", color: "rgba(150, 150, 150, 1)" }}>
-                <div style={{ }}> <span dangerouslySetInnerHTML={{ __html: appointmentData.description ? appointmentData.description : "<i>nothing to see here</i>" }} /> </div>
+            <Grid item
+                xs={10}
+                style={{ marginLeft: 30, paddingTop: 10,  borderTop: "1px solid rgba(200, 200, 200, 1)", color: "rgba(150, 150, 150, 1)" }}>
+                <div> <span dangerouslySetInnerHTML={{ __html: appointmentData.description ? appointmentData.description : "<i>nothing to see here</i>" }} /> </div>
             </Grid>
         </Grid>
     </AppointmentTooltip.Content>
 );
+Content.propTypes = {
+    appointmentData: PropTypes.object
+};
 const Appointment = ({ children, style, ...restProps }) => (
     <Appointments.Appointment
         {...restProps}
@@ -31,9 +39,13 @@ const Appointment = ({ children, style, ...restProps }) => (
             backgroundColor: theme.primary[1],
             borderRadius: 8,
         }}>
-            {children}
+        {children}
     </Appointments.Appointment>
 );
+Appointment.propTypes = {
+    children: PropTypes.object,
+    style: PropTypes.object
+};
 class Schedule extends Component {
     UNSAFE_componentWillMount() {
         this.setState({ events: [] });
@@ -52,7 +64,6 @@ class Schedule extends Component {
             }, (error, response, body) => {
                 if (!error && body.statusCode === 200) {
                     let events = [];
-                    let resources = [];
                     for (let i = 0; i < body.body.length; i++) {
                         events.push({
                             title: body.body[i].summary,
@@ -79,22 +90,22 @@ class Schedule extends Component {
                 <div style={{ width: "100%", textAlign: "left" }}>
                     <ListGroup className="live-container"
                         flush>
-                            <Scheduler
-                                data={this.state.events}>
-                                <ViewState
-                                    currentDate={"11-07-2020"}
-                                />
-                                <DayView
-                                    startDayHour={(new Date()).getHours()}
-                                    endDayHour={(new Date()).getHours() + 6 > 24 ? 24 : (new Date()).getHours() + 4}
-                                />
-                                <Appointments
-                                    appointmentComponent={Appointment}
-                                />
-                                <AppointmentTooltip
-                                    contentComponent={Content}
-                                />
-                            </Scheduler>
+                        <Scheduler
+                            data={this.state.events}>
+                            <ViewState
+                                currentDate={"11-07-2020"}
+                            />
+                            <DayView
+                                startDayHour={(new Date()).getHours()}
+                                endDayHour={(new Date()).getHours() + 6 > 24 ? 24 : (new Date()).getHours() + 4}
+                            />
+                            <Appointments
+                                appointmentComponent={Appointment}
+                            />
+                            <AppointmentTooltip
+                                contentComponent={Content}
+                            />
+                        </Scheduler>
                     </ListGroup>
                 </div>
             </div>
