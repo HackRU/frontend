8c2a1376b002b99bc1b10f333468a8bb5fa138f8
Zain Ali <zain08816@gmail.com>
2021-04-16 12:52:51 -0600

    Add current schedule

--- src/components/Landing/Sections/Schedule.jsx
+++ src/components/Landing/Sections/Schedule.jsx
@@ -1,54 +1,52 @@
 import React from "react";
 import { theme } from "../../../Defaults";
-import {  Typography } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
 import Card from "../../Card";
-// import { ViewState } from "@devexpress/dx-react-scheduler";
-// import { Scheduler, DayView, Appointments } from "@devexpress/dx-react-scheduler-material-ui";
+import { ViewState } from "@devexpress/dx-react-scheduler";
+import { Scheduler, DayView, Appointments } from "@devexpress/dx-react-scheduler-material-ui";
 import PropTypes from "prop-types";
 /**
  * Schedule component for the landing page
  */
 function Schedule() {
-    // const Appointment = ({ children, style, ...restProps }) => (
-    //     <Appointments.Appointment
-    //         {...restProps}
-    //         style={{
-    //             ...style,
-    //             backgroundColor: theme.primary[1],
-    //             borderRadius: 8,
-    //         }}>
-    //         {children}
-    //     </Appointments.Appointment>
-    // );
+    const Appointment = ({ children, style, ...restProps }) => (
+        <Appointments.Appointment
+            {...restProps}
+            style={{
+                ...style,
+                backgroundColor: theme.primary[1],
+                borderRadius: 8,
+            }}>
+            {children}
+        </Appointments.Appointment>
+    );
     
-    // const schedule = [{
-    //     "date": "2021-04-17",
-    //     "events": [
-    //         { startDate: "2020-11-07T10:00", endDate: "2020-11-07T11:00", title: "Check-in" },
-    //         { startDate: "2020-11-07T11:30", endDate: "2020-11-07T13:00", title: "Opening Ceremony" },
-    //         { startDate: "2020-11-07T13:00", endDate: "2020-11-08T12:30", title: "Hacking"},
-    //         { startDate: "2020-11-07T15:00", endDate: "2020-11-07T15:20", title: "Blueprint Presents: Introduction to Figma"},
-    //         { startDate: "2020-11-07T15:30", endDate: "2020-11-07T16:30", title: "MLH CTF: Join MLH in a capture the flag style event"},
-    //         { startDate: "2020-11-07T16:30", endDate: "2020-11-07T17:30", title: "RUMAD Presents: Introduction to Kotlin and Android Studio"},
-    //         { startDate: "2020-11-07T18:00", endDate: "2020-11-07T19:00", title: "MLH Bob Ross: Join MLH in recreating one of Bob Ross's paintings in MS Paint"},
-    //         { startDate: "2020-11-07T19:00", endDate: "2020-11-07T20:00", title: "WICS Live Panel: Join us for a Q&A panel with WICS (Women in Computer Science)"},
-    //         { startDate: "2020-11-07T20:00", endDate: "2020-11-07T21:00", title: "Cog Sci Workshop"}
-    //     ]
-    // }, {
-    //     "date": "2021-04-18",
-    //     "events": [
-    //         { startDate: "2020-11-07T13:00", endDate: "2020-11-08T12:30", title: "Hacking"},
-    //         { startDate: "2020-11-08T13:30", endDate: "2020-11-08T15:00", title: "Judging"},
-    //         { startDate: "2020-11-08T15:00", endDate: "2020-11-08T16:00", title: "Presentations"},
-    //         { startDate: "2020-11-08T16:00", endDate: "2020-11-08T17:00", title: "Closing Ceremony"},
-    //     ]
-    // }];
+    const schedule = [{
+        "date": "2021-04-17",
+        "events": [
+            { startDate: "2021-04-17T10:00", endDate: "2021-04-17T11:00", title: "Check-in" },
+            { startDate: "2021-04-17T11:30", endDate: "2021-04-17T13:00", title: "Opening Ceremony" },
+            { startDate: "2021-04-17T13:00", endDate: "2021-04-18T12:59", title: "Hacking"},
+            { startDate: "2021-04-17T16:00", endDate: "2021-04-17T16:45", title: "Blueprint Presents: Introduction to Flutter"},
+            { startDate: "2021-04-17T17:00", endDate: "2021-04-17T18:00", title: "Rutgers QFC Presents: Dimensionality Reduction and Manifold Learning"},
+            { startDate: "2021-04-17T19:30", endDate: "2021-04-17T20:00", title: "WICS Live Panel: Join us for a Q&A panel with WICS (Women in Computer Science)"},
+            { startDate: "2021-04-17T20:15", endDate: "2021-04-17T21:15", title: "HackRU CTF"},
+            { startDate: "2021-04-17T22:00", endDate: "2021-04-17T22:45", title: "RU COGS Presents: Game Dev 101"},
+        ]
+    }, {
+        "date": "2021-04-18",
+        "events": [
+            { startDate: "2021-04-17T13:00", endDate: "2021-04-18T12:59", title: "Hacking"},
+            { startDate: "2021-04-18T13:00", endDate: "2021-04-18T15:00", title: "Judging"},
+            { startDate: "2021-04-18T15:30", endDate: "2021-04-18T16:00", title: "Closing Ceremony"},
+        ]
+    }];
     return (
         <Card backgroundColor={"white"}
             sideBar={theme.primary[1]}>
             <h1 style={{color: "black"}}
                 className="display-4 theme-font">Schedule</h1>
-            {/* <div className="row mb-3"
+            <div className="row mb-3"
                 style={{ marginLeft: -50, marginRight: -50, paddingLeft: 25 }}>
                 <Grid container
                     spacing={3}>
@@ -64,7 +62,7 @@ function Schedule() {
                                 />
                                 <DayView
                                     startDayHour={9}
-                                    endDayHour={22}
+                                    endDayHour={23}
                                 />
                                 <Appointments
                                     appointmentComponent={Appointment}
@@ -73,10 +71,7 @@ function Schedule() {
                         </Grid>
                     )}
                 </Grid>
-            </div> */}
-            <Typography style={{ color: "black" }}>
-                To be announced! Check again soon!
-            </Typography>
+            </div>
         </Card>
     );
 }
