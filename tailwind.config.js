85af3d4e08998c634d0ec88f78940e7c849574be
Kevin Monisit <monisitkevin@gmail.com>
2023-01-19 13:50:19 -0500

    Isolated TailwindCSS config to new landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        "./src/**/*.{js,jsx,ts,tsx}",
+        "./src/components/_Landing/**/*.{js,jsx,ts,tsx}",
     ],
     theme: {
         extend: {
