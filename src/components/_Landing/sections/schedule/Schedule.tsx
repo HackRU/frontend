3d0a10708c929e93fc44342b8bdb9accd133b2a4
Kevin Monisit <monisitkevin@gmail.com>
2023-02-19 16:13:06 -0500

    Schedule adjustments (#533)
    * Fixed padding issues
    
    * Modified line
    
    * schedule changes
    
    ---------
    
    Co-authored-by: GabrielWeinbrenner <gabriel.weinbrenner@gmail.com>

--- src/components/_Landing/sections/schedule/Schedule.tsx
+++ src/components/_Landing/sections/schedule/Schedule.tsx
@@ -9,15 +9,14 @@ interface DayInfo {
     times: { time: string, event: string, reactIcon?: JSX.Element }[]
 }
 
-const schedule: DayInfo[] = [
-    {
+const schedule: { [day: string]: DayInfo } = {
+    "Saturday": {
         "day": "Saturday",
         "times": [
             { "time": "2:00 PM", "event": "Check-in starts" },
             { "time": "3:00 PM", "event": "Opening Ceremony", "reactIcon": <MdCelebration /> },
-            { "time": "3:25 PM", "event": "Team Building Event" },
+            { "time": "4:00 PM", "event": "Team Building Event" },
             { "time": "4:00 PM", "event": "Hacking Starts", "reactIcon": <FaHourglassStart /> },
-            { "time": "4:25 PM", "event": "Event" },
             { "time": "6:00 PM", "event": "Rutgers Ethitech Presentation" },
             { "time": "7:00 PM", "event": "WiCS Presentation" },
             { "time": "8:00 PM", "event": "Dinner & Another Event", "reactIcon": <FaPizzaSlice /> },
@@ -25,23 +24,23 @@ const schedule: DayInfo[] = [
         ],
     },
 
-    {
+    "Sunday": {
         "day": "Sunday",
         "times": [
             { "time": "8:00 AM", "event": "Breakfast", "reactIcon": <MdFreeBreakfast /> },
             { "time": "9:00 AM", "event": "RUMad Presentation" },
-            { "time": "11:00 AM", "event": "Event" },
+            // { "time": "11:00 AM", "event": "Event" },
             { "time": "12:00 PM", "event": "Lunch", "reactIcon": <MdLunchDining /> },
             { "time": "5:00 PM", "event": "Submissions Due", "reactIcon": <BsFillExclamationTriangleFill /> },
             { "time": "5:30 PM", "event": "Submissions Due (Hard Deadline)" },
-            { "time": "6:30 PM", "event": "Judging Begins" },
+            { "time": "6:00 PM", "event": "Judging Begins" },
             { "time": "7:00 PM", "event": "Dinner", "reactIcon": <FaUtensils /> },
             { "time": "7:30 PM", "event": "Judging Ends" },
             { "time": "8:00 PM", "event": "Closing Ceremony", "reactIcon": <MdCelebration /> },
             // { "time": "9:00 PM", "event": "Venue closes" },
         ],
     }
-];
+};
 
 function Fox() {
     return (
@@ -66,10 +65,10 @@ function ScheduleOfTheDay(props: { dayInfo: DayInfo }) {
             <div className="text-5xl md:text-7xl w-full text-center mb-4 font-semibold glow-subtitles text-textSubtitle">{dayInfo.day}</div>
             <div className="w-full">
                 {times.map((timeInfo, index) => (
-                    <div className="flex flex-row w-full text-xl font-semibold my-2 md:my-5 md:px-3 pr-4"
+                    <div className="flex flex-row w-full text-xl my-2 md:my-5 md:px-3 pr-4"
                         key={`${day}-${index}`}
                     >
-                        <div className="w-2/5 h-fit text-right pr-5">{timeInfo.time}</div>
+                        <div className="w-2/5 h-fit text-right pr-2 font-black">{timeInfo.time}</div>
                         <div className="w-3/5">
                             {timeInfo.event}
                             &nbsp;&nbsp;
@@ -97,14 +96,11 @@ function Schedule() {
             <div className="w-full max-w-7xl h-fit flex flex-col items-center">
                 <div className="text-7xl text-text glow-subtitles font-semibold z-30">Schedule</div>
 
-                <div className="transparent-black-background w-full text-text rounded-3xl mt-10 flex flex-col
-                                md:flex-row">
-                    {schedule.map((dayInfo, index) => (
-                        <ScheduleOfTheDay
-                            dayInfo={dayInfo}
-                            key={index}
-                        />
-                    ))}
+                <div className="transparent-black-background w-full text-text rounded-3xl mt-10
+                                flex flex-col items-center md:flex-row md:items-start relative">
+                    <ScheduleOfTheDay dayInfo={schedule["Saturday"]} />
+                    <div className="w-20 h-2 bg-text md:invisible md:absolute glow-subtitle-color rounded-sm" />
+                    <ScheduleOfTheDay dayInfo={schedule["Sunday"]} />
                 </div>
             </div>
         </div>
