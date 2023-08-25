52c4a4797b316c1864a20a51a6c89e63426b4aed
Kevin Monisit <monisitkevin@gmail.com>
2023-08-25 14:11:40 -0400

    Modified positioning

--- src/components/_Landing/sections/footer/Footer.tsx
+++ src/components/_Landing/sections/footer/Footer.tsx
@@ -11,33 +11,38 @@ import React from "react";
 function Footer() {
     return (
         <div className="text-white flex flex-col items-center py-2">
-            <div className="w-full pb-4 flex justify-center">
-                <a href="mailto:info@hackru.org"
-                    className="social-links"
-                    target="_blank"
-                    style={{ display: "inline-block", marginRight: "1vh" }}
-                    rel="noopener noreferrer"><MdEmail size="1.5em" />
-                </a>
-                <a href="https://www.facebook.com/theHackRU/"
-                    className="social-links"
-                    target="_blank"
-                    style={{ display: "inline-block", marginRight: "1vh" }}
-                    rel="noopener noreferrer"><FaFacebookF size="1.5em" />
-                </a>
-                <a href="https://www.instagram.com/thehackru/"
-                    className="social-links"
-                    target="_blank"
-                    style={{ display: "inline-block", marginRight: "1vh" }}
-                    rel="noopener noreferrer"><FaInstagram size="1.5em" />
-                </a>
-                <a href="https://twitter.com/theHackRU"
-                    className="social-links"
-                    target="_blank"
-                    style={{ display: "inline-block", marginRight: "" }}
-                    rel="noopener noreferrer"><FaTwitter size="1.5em" />
-                </a>
+            <div className="pb-4 flex-col justify-center">
+                <div>
+                    <a href="mailto:info@hackru.org"
+                        className="social-links"
+                        target="_blank"
+                        style={{ display: "inline-block", marginRight: "1vh" }}
+                        rel="noopener noreferrer"><MdEmail size="1.5em" />
+                    </a>
+                    <a href="https://www.facebook.com/theHackRU/"
+                        className="social-links"
+                        target="_blank"
+                        style={{ display: "inline-block", marginRight: "1vh" }}
+                        rel="noopener noreferrer"><FaFacebookF size="1.5em" />
+                    </a>
+                    <a href="https://www.instagram.com/thehackru/"
+                        className="social-links"
+                        target="_blank"
+                        style={{ display: "inline-block", marginRight: "1vh" }}
+                        rel="noopener noreferrer"><FaInstagram size="1.5em" />
+                    </a>
+                    <a href="https://twitter.com/theHackRU"
+                        className="social-links"
+                        target="_blank"
+                        style={{ display: "inline-block", marginRight: "" }}
+                        rel="noopener noreferrer"><FaTwitter size="1.5em" />
+                    </a>
+                </div>
+                <div>
+                    <a className="text-subtitles  font-semibold pl-2" href="https://linktr.ee/thehackru">linktr.ee/thehackru</a>
+
+                </div>
             </div>
-            <a className="text-subtitles glow-subtitles font-semibold" href="https://linktr.ee/thehackru">https://linktr.ee/thehackru</a>
             <div className="text-subtitles glow-subtitles font-semibold">Ⓒ HackRU 2023</div>
         </div>
     );
