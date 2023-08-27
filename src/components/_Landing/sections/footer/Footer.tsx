ff66d899138cbe7922a3481d67dd0a3766057127
dchaudhari0811 <dchaudhari2003@gmail.com>
2023-08-27 00:22:24 -0400

    removed medium link and added linktree (#559)
    * removed medium link and added linktree
    
    * removed medium import
    
    * Modified positioning
    
    ---------
    
    Co-authored-by: Kevin Monisit <monisitkevin@gmail.com>

--- src/components/_Landing/sections/footer/Footer.tsx
+++ src/components/_Landing/sections/footer/Footer.tsx
@@ -1,7 +1,6 @@
 
 import { FaFacebookF } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
-import { FaMediumM } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 import React from "react";
@@ -12,37 +11,37 @@ import React from "react";
 function Footer() {
     return (
         <div className="text-white flex flex-col items-center py-2">
-            <div className="w-full pb-4 flex justify-center">
-                <a href="mailto:info@hackru.org"
-                    className="social-links"
-                    target="_blank"
-                    style={{ display: "inline-block", marginRight: "1vh" }}
-                    rel="noopener noreferrer"><MdEmail size="1.5em" />
-                </a>
-                <a href="https://www.facebook.com/theHackRU/"
-                    className="social-links"
-                    target="_blank"
-                    style={{ display: "inline-block", marginRight: "1vh" }}
-                    rel="noopener noreferrer"><FaFacebookF size="1.5em" />
-                </a>
-                <a href="https://www.instagram.com/thehackru/"
-                    className="social-links"
-                    target="_blank"
-                    style={{ display: "inline-block", marginRight: "1vh" }}
-                    rel="noopener noreferrer"><FaInstagram size="1.5em" />
-                </a>
-                <a href="https://medium.com/the-hackru"
-                    className="social-links"
-                    target="_blank"
-                    style={{ display: "inline-block", marginRight: "1vh" }}
-                    rel="noopener noreferrer"><FaMediumM size="1.5em" />
-                </a>
-                <a href="https://twitter.com/theHackRU"
-                    className="social-links"
-                    target="_blank"
-                    style={{ display: "inline-block", marginRight: "1vh" }}
-                    rel="noopener noreferrer"><FaTwitter size="1.5em" />
-                </a>
+            <div className="pb-4 flex-col justify-center">
+                <div>
+                    <a href="mailto:info@hackru.org"
+                        className="social-links"
+                        target="_blank"
+                        style={{ display: "inline-block", marginRight: "1vh" }}
+                        rel="noopener noreferrer"><MdEmail size="1.5em" />
+                    </a>
+                    <a href="https://www.facebook.com/theHackRU/"
+                        className="social-links"
+                        target="_blank"
+                        style={{ display: "inline-block", marginRight: "1vh" }}
+                        rel="noopener noreferrer"><FaFacebookF size="1.5em" />
+                    </a>
+                    <a href="https://www.instagram.com/thehackru/"
+                        className="social-links"
+                        target="_blank"
+                        style={{ display: "inline-block", marginRight: "1vh" }}
+                        rel="noopener noreferrer"><FaInstagram size="1.5em" />
+                    </a>
+                    <a href="https://twitter.com/theHackRU"
+                        className="social-links"
+                        target="_blank"
+                        style={{ display: "inline-block", marginRight: "" }}
+                        rel="noopener noreferrer"><FaTwitter size="1.5em" />
+                    </a>
+                </div>
+                <div>
+                    <a className="text-subtitles  font-semibold pl-2" href="https://linktr.ee/thehackru">linktr.ee/thehackru</a>
+
+                </div>
             </div>
             <div className="text-subtitles glow-subtitles font-semibold">Ⓒ HackRU 2023</div>
         </div>
