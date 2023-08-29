8b9ce3759b037af9ed255bb4e0afe8cc0d5cee43
Kevin Monisit <monisitkevin@gmail.com>
2023-08-29 17:00:09 -0400

    Added clouds and whale

--- src/components/_Landing/sections/hero/hero.tsx
+++ src/components/_Landing/sections/hero/hero.tsx
@@ -3,11 +3,11 @@ import React from "react";
 import MainHeroContent from "./MainHeroContent";
 import "./hero.css";
 import Navbar from "./Navbar";
-import rabbit from "../../assets/suminRabbit2.png";
-import cloud from "../../assets/clouds/cloud1.svg";
-import cloud2 from "../../assets/clouds/cloud2.svg";
-import cloud3 from "../../assets/clouds/cloud3.svg";
-import cloud4 from "../../assets/clouds/cloud4.svg";
+import rabbit from "../../assets/whale.png";
+import cloud from "../../assets/clouds/cloud1.png";
+import cloud2 from "../../assets/clouds/cloud2.png";
+import cloud3 from "../../assets/clouds/cloud3.png";
+import cloud4 from "../../assets/clouds/cloud4.png";
 import { FaArrowDown } from "react-icons/fa";
 import { scrollToSectionName, useUserScrolled } from "./utilities";
 import { Profile } from "../../../Profile";
@@ -60,14 +60,14 @@ function Hero(props: { profile: Profile }) {
                 <img src={cloud2}
                     alt="cloud" />
             </div>
-
+            {/*
             <div className="absolute z-0 w-96 top-[10rem] left-[45rem]
        opacity-50 md:opacity-80 md:top-[30rem] md:left-[40rem]
        lg:top-[40rem] lg:left-[50rem]
        lg:w-144 cloud">
                 <img src={cloud3}
                     alt="cloud" />
-            </div>
+            </div> */}
 
             <div className="absolute z-0 w-96 top-[10rem] left-[-10rem]
        opacity-50 md:opacity-80 md:top-[30rem] md:left-[-10rem]
