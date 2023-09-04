3cd466272fda34a5de7e2ac16ce0447c1fc252e9
Kevin Monisit <monisitkevin@gmail.com>
2023-09-04 09:51:06 -0400

    Added new skunk asset to FAQs

--- src/components/_Landing/sections/faq/FAQ.tsx
+++ src/components/_Landing/sections/faq/FAQ.tsx
@@ -3,6 +3,7 @@ import QuestionContainer from "./QuestionContainer";
 import cloud from "../../assets/clouds/cloud1.png";
 import cloud3 from "../../assets/clouds/cloud3.png";
 import owl from "../../assets/animals/owl-min.png";
+import skunk from "../../assets/animals/skunkIslandv2-min.png";
 
 function FAQ() {
     return (
@@ -22,6 +23,17 @@ function FAQ() {
                     lg:w-[32rem] lg:-left-[10rem] lg:-top-[8rem]
                     xl:w-[40rem] xl:-left-[15rem] xl:-top-[8rem]
                     floating" />
+
+                <img src={skunk}
+                    loading="lazy"
+                    alt="skunk"
+                    className="absolute w-[17rem] z-20
+                    -right-[2rem] bottom-[8rem]
+                    md:w-[23rem]
+                    lg:w-[26rem] lg:-right-[5rem] lg:-top-[2rem]
+                    xl:w-[30rem] xl:-right-[9rem] xl:top-[3rem]
+                    floating" />
+
                 <img src={cloud}
                     className="absolute cloud w-[24rem] z-10" />
                 <img src={cloud3}
