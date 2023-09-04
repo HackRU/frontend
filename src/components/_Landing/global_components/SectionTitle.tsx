c42e01100f5b0b336a5d25c1b83559bfad9a323c
Kevin Monisit <monisitkevin@gmail.com>
2023-09-04 10:32:57 -0400

    Adjusted section titles and fixed navbar

--- src/components/_Landing/global_components/SectionTitle.tsx
+++ src/components/_Landing/global_components/SectionTitle.tsx
@@ -5,7 +5,7 @@ function SectionTitle(props: { title: string }) {
     return (
         <div className="flex w-full px-4 justify-center items-center my-10 z-50">
             <div className="w-1/6 h-2 bg-text mr-4" />
-            <div className="HeaderText font-semibold text-7xl text-text">{title}</div>
+            <div className="HeaderText font-semibold text-4xl sm:text-6xl md:text-7xl text-text">{title}</div>
             <div className="w-1/6 h-2 bg-text ml-4" />
         </div>
     );
