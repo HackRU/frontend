603fbaeb1e66f9b07a7c56981fcf976ef9300c1e
dchaudhari0811 <dchaudhari2003@gmail.com>
2023-09-18 20:51:28 -0400

    gellery for highlights & winners

---
+++ src/components/_Landing/sections/Gallery/Gallery.tsx
@@ -0,0 +1,185 @@
+import React from "react";
+
+function Gallery() {
+    return (
+        <div>
+            <div className="container mx-auto m-6 lg:px-32 lg:pt-12">
+                <p className = "text-center mb-8 text-3xl font-semibold"
+                    style = {{color:"#f1e192", fontFamily:"titillium-web"}}>Highlights</p>
+                <div className="-m-1 flex flex-wrap md:-m-2">
+                    <div className="flex w-1/5 h-2 mb-14 md:mb-0 xl:mb-8 md:h-36 xs:h-4 flex-wrap hover:scale-105">
+                        <div className="w-full p-2 md:p-2">
+                            <img
+                                alt="gallery"
+                                className="block h-2/3 w-full rounded-lg object-cover object-center"
+                                src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*" />
+                        </div>
+                    </div>
+                    <div className="flex w-1/5 h-2 md:h-36 xs:h-4 flex-wrap hover:scale-105">
+                        <div className="w-full p-2 md:p-2">
+                            <img
+                                alt="gallery"
+                                className="block h-2/3 w-full rounded-lg object-cover object-center"
+                                src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*" />
+                        </div>
+                    </div>
+                    <div className="flex w-1/5 h-2 md:h-36 xs:h-4 flex-wrap hover:scale-105">
+                        <div className="w-full p-2 md:p-2">
+                            <img
+                                alt="gallery"
+                                className="block h-2/3 w-full rounded-lg object-cover object-center"
+                                src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*" />
+                        </div>
+                    </div>
+                    <div className="flex w-1/5 h-2 md:h-36 xs:h-4 flex-wrap hover:scale-105">
+                        <div className="w-full p-2 md:p-2">
+                            <img
+                                alt="gallery"
+                                className="block h-2/3 w-full rounded-lg object-cover object-center"
+                                src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*" />
+                        </div>
+                    </div>
+                    <div className="flex w-1/5 h-2 md:h-36 xs:h-4 flex-wrap hover:scale-105">
+                        <div className="w-full p-2 md:p-2">
+                            <img
+                                alt="gallery"
+                                className="block h-2/3 w-full rounded-lg object-cover object-center hover:scale-105"
+                                src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*" />
+                        </div>
+                    </div>
+                    <div className="flex w-1/5 h-2 md:h-36 xl:mb-14 xs:h-4 flex-wrap hover:scale-105">
+                        <div className="w-full p-2 md:p-2">
+                            <img
+                                alt="gallery"
+                                className="block h-2/3 w-full rounded-lg object-cover object-center hover:scale-105"
+                                src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*" />
+                        </div>
+                    </div>
+                    <div className="flex w-1/5 h-2 md:h-36 xs:h-4 flex-wrap hover:scale-105">
+                        <div className="w-full p-2 md:p-2">
+                            <img
+                                alt="gallery"
+                                className="block h-2/3 w-full rounded-lg object-cover object-center hover:scale-105"
+                                src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*" />
+                        </div>
+                    </div>
+                    <div className="flex w-1/5 h-2 md:h-36 xs:h-4 flex-wrap hover:scale-105">
+                        <div className="w-full p-2 md:p-2">
+                            <img
+                                alt="gallery"
+                                className="block h-2/3 w-full rounded-lg object-cover object-center hover:scale-105"
+                                src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*" />
+                        </div>
+                    </div>
+                    <div className="flex w-1/5 h-2 md:h-36 xs:h-4 flex-wrap hover:scale-105">
+                        <div className="w-full p-2 md:p-2">
+                            <img
+                                alt="gallery"
+                                className="block h-2/3 w-full rounded-lg object-cover object-center hover:scale-105"
+                                src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*" />
+                        </div>
+                    </div>
+                    <div className="flex w-1/5 h-2 mb-14 md:mb-0 xl:mb-8 md:h-36 xs:h-4 flex-wrap hover:scale-105">
+                        <div className="w-full p-2 md:p-2">
+                            <img
+                                alt="gallery"
+                                className="block h-2/3 w-full rounded-lg object-cover object-center hover:scale-105"
+                                src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*" />
+                        </div>
+                    </div>
+   
+    
+                </div>
+                <p className = "text-center mb-8 mt-8 text-4xl font-semibold"
+                    style = {{color:"#f1e192", fontFamily:"titillium-web"}}>Winners</p>
+                <div className="-m-1 flex flex-wrap md:-m-2">
+                    <div className="flex w-1/5 h-2 mb-14 md:mb-0 xl:mb-8 md:h-36 xs:h-4 flex-wrap hover:scale-105">
+                        <div className="w-full p-2 md:p-2">
+                            <img
+                                alt="gallery"
+                                className="block h-2/3 w-full rounded-lg object-cover object-center hover:scale-105"
+                                src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*" />
+                        </div>
+                    </div>
+                    <div className="flex w-1/5 h-2 md:h-36 xs:h-4 flex-wrap hover:scale-105">
+                        <div className="w-full p-2 md:p-2">
+                            <img
+                                alt="gallery"
+                                className="block h-2/3 w-full rounded-lg object-cover object-center hover:scale-105"
+                                src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*" />
+                        </div>
+                    </div>
+                    <div className="flex w-1/5 h-2 md:h-36 xs:h-4 flex-wrap hover:scale-105">
+                        <div className="w-full p-2 md:p-2">
+                            <img
+                                alt="gallery"
+                                className="block h-2/3 w-full rounded-lg object-cover object-center hover:scale-105"
+                                src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*" />
+                        </div>
+                    </div>
+                    <div className="flex w-1/5 h-2 md:h-36 xs:h-4 flex-wrap hover:scale-105">
+                        <div className="w-full p-2 md:p-2">
+                            <img
+                                alt="gallery"
+                                className="block h-2/3 w-full rounded-lg object-cover object-center hover:scale-105"
+                                src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*" />
+                        </div>
+                    </div>
+                    <div className="flex w-1/5 h-2 md:h-36 xs:h-4 flex-wrap hover:scale-105">
+                        <div className="w-full p-2 md:p-2">
+                            <img
+                                alt="gallery"
+                                className="block h-2/3 w-full rounded-lg object-cover object-center hover:scale-105"
+                                src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*" />
+                        </div>
+                    </div>
+                    <div className="flex w-1/5 h-2 md:h-36 xs:h-4 flex-wrap hover:scale-105">
+                        <div className="w-full p-2 md:p-2">
+                            <img
+                                alt="gallery"
+                                className="block h-2/3 w-full rounded-lg object-cover object-center hover:scale-105"
+                                src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*" />
+                        </div>
+                    </div>
+                    <div className="flex w-1/5 h-2 md:h-36 xs:h-4 flex-wrap hover:scale-105">
+                        <div className="w-full p-2 md:p-2">
+                            <img
+                                alt="gallery"
+                                className="block h-2/3 w-full rounded-lg object-cover object-center hover:scale-105"
+                                src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*" />
+                        </div>
+                    </div>
+                    <div className="flex w-1/5 h-2 md:h-36 xs:h-4 flex-wrap hover:scale-105">
+                        <div className="w-full p-2 md:p-2">
+                            <img
+                                alt="gallery"
+                                className="block h-2/3 w-full rounded-lg object-cover object-center hover:scale-105"
+                                src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*" />
+                        </div>
+                    </div>
+                    <div className="flex w-1/5 h-2 md:h-36 xs:h-4 flex-wrap hover:scale-105">
+                        <div className="w-full p-2 md:p-2">
+                            <img
+                                alt="gallery"
+                                className="block h-2/3 w-full rounded-lg object-cover object-center hover:scale-105"
+                                src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*" />
+                        </div>
+                    </div>
+                    <div className="flex w-1/5 h-2 mb-14 md:mb-1 xl:mb-8 md:h-36 xs:h-4 flex-wrap hover:scale-105">
+                        <div className="w-full p-2 md:p-2">
+                            <img
+                                alt="gallery"
+                                className="block h-2/3 w-full rounded-lg object-cover object-center hover:scale-105"
+                                src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*" />
+                        </div>
+                    </div>
+    
+    
+                </div>
+            </div>
+        </div>
+    );
+}
+
+ 
+export default Gallery;
